e9a05976a8262d2a50a6bbc057f925f7d55ac8d1
bahoang84 <bahoang84@gmail.com>
2021-12-16 13:43:34 +0700

    Xem chi tiet quan ly kh mua truc tiep dành cho cap cuc

--- src/main/java/com/tcdt/qlnvhang/controller/QlnvQdMuattController.java
+++ src/main/java/com/tcdt/qlnvhang/controller/QlnvQdMuattController.java
@@ -7,11 +7,15 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.hibernate.Filter;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -32,9 +36,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tcdt.qlnvhang.enums.EnumResponse;
 import com.tcdt.qlnvhang.repository.QlnvDxkhMuaTtHdrRepository;
+import com.tcdt.qlnvhang.repository.QlnvQdMuattHdr2Repository;
 import com.tcdt.qlnvhang.repository.QlnvQdMuattHdrRepository;
 import com.tcdt.qlnvhang.request.IdSearchReq;
 import com.tcdt.qlnvhang.request.StatusReq;
+import com.tcdt.qlnvhang.request.StrSearchReq;
 import com.tcdt.qlnvhang.request.object.QlnvQdMuattDtlReq;
 import com.tcdt.qlnvhang.request.object.QlnvQdMuattHdrReq;
 import com.tcdt.qlnvhang.request.search.QlnvQdMuattSearchAdjustReq;
@@ -43,6 +49,8 @@ import com.tcdt.qlnvhang.response.BaseResponse;
 import com.tcdt.qlnvhang.table.QlnvQdMuattDtl;
 import com.tcdt.qlnvhang.table.QlnvQdMuattDtlCtiet;
 import com.tcdt.qlnvhang.table.QlnvQdMuattHdr;
+import com.tcdt.qlnvhang.table.QlnvQdMuattHdr2;
+import com.tcdt.qlnvhang.table.catalog.QlnvDmDonvi;
 import com.tcdt.qlnvhang.util.Contains;
 import com.tcdt.qlnvhang.util.Doc4jUtils;
 import com.tcdt.qlnvhang.util.Maps;
@@ -65,6 +73,12 @@ public class QlnvQdMuattController extends BaseController {
 
 	@Autowired
 	private QlnvDxkhMuaTtHdrRepository qlnvDxkhMuaTtHdrRepository;
+
+	@Autowired
+	private QlnvQdMuattHdr2Repository qdMuaHangHdr2Repository;
+
+	@Autowired
+	private EntityManager entityManager;
 
 	@ApiOperation(value = "Tạo mới quyết định mua trực tiếp", response = List.class)
 	@PostMapping(value = PathContains.URL_TAO_MOI, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -384,8 +398,8 @@ public class QlnvQdMuattController extends BaseController {
 			if (objReq.getLoaiDchinh() == null || !Contains.mappingLoaiDc.containsKey(objReq.getLoaiDchinh()))
 				throw new Exception("Loại điều chỉnh không phù hợp");
 
-			Optional<QlnvQdMuattHdr> opCheck = qdMuaHangHdrRepository
-					.findBySoQdinhGocAndTrangThai(objReq.getSoQdinhGoc(), Contains.DUYET);
+			Optional<QlnvQdMuattHdr> opCheck = qdMuaHangHdrRepository.findBySoQdinhAndTrangThai(objReq.getSoQdinhGoc(),
+					Contains.DUYET);
 			if (!opCheck.isPresent())
 				throw new Exception("Quyết định gốc không tồn tại hoặc đã bị điều chỉnh");
 
@@ -440,8 +454,8 @@ public class QlnvQdMuattController extends BaseController {
 			if (objReq.getLoaiDchinh() == null || !Contains.mappingLoaiDc.containsKey(objReq.getLoaiDchinh()))
 				throw new Exception("Loại điều chỉnh không phù hợp");
 
-			Optional<QlnvQdMuattHdr> opCheck = qdMuaHangHdrRepository
-					.findBySoQdinhGocAndTrangThai(objReq.getSoQdinhGoc(), Contains.DUYET);
+			Optional<QlnvQdMuattHdr> opCheck = qdMuaHangHdrRepository.findBySoQdinhAndTrangThai(objReq.getSoQdinhGoc(),
+					Contains.DUYET);
 			if (!opCheck.isPresent())
 				throw new Exception("Quyết định gốc không tồn tại hoặc đã bị điều chỉnh");
 
@@ -485,4 +499,125 @@ public class QlnvQdMuattController extends BaseController {
 		return ResponseEntity.ok(resp);
 	}
 
+	@ApiOperation(value = "Xem chi tiết thông tin quyết định gốc mua trực tiếp", response = List.class)
+	@GetMapping(value = PathContains.URL_CHI_TIET_GOC, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<BaseResponse> detailFather(HttpServletRequest request,
+			@Valid @RequestBody StrSearchReq objReq) {
+		BaseResponse resp = new BaseResponse();
+		try {
+			if (StringUtils.isEmpty(objReq.getStr()))
+				throw new UnsupportedOperationException("Không tồn tại bản ghi");
+			QlnvQdMuattHdr objHdr = qdMuaHangHdrRepository.findBySoQdinh(objReq.getStr());
+			if (ObjectUtils.isEmpty(objHdr))
+				throw new UnsupportedOperationException("Không tồn tại bản ghi");
+			resp.setData(objHdr);
+			resp.setStatusCode(EnumResponse.RESP_SUCC.getValue());
+			resp.setMsg(EnumResponse.RESP_SUCC.getDescription());
+		} catch (Exception e) {
+			resp.setStatusCode(EnumResponse.RESP_FAIL.getValue());
+			resp.setMsg(e.getMessage());
+			log.error(e.getMessage());
+		}
+		return ResponseEntity.ok(resp);
+	}
+
+	@ApiOperation(value = "Tra cứu quyết định mua trực tiếp dành cho cấp cục", response = List.class)
+	@PostMapping(value = PathContains.URL_TRA_CUU
+			+ PathContains.URL_CAP_CUC, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<BaseResponse> colectionChild(HttpServletRequest request,
+			@Valid @RequestBody QlnvQdMuattSearchReq objReq) {
+		BaseResponse resp = new BaseResponse();
+		try {
+			int page = PaginationSet.getPage(objReq.getPaggingReq().getPage());
+			int limit = PaginationSet.getLimit(objReq.getPaggingReq().getLimit());
+			Pageable pageable = PageRequest.of(page, limit, Sort.by("id").ascending());
+
+			// Lay thong tin don vi quan ly
+			QlnvDmDonvi objDvi = getDvi(request);
+			if (ObjectUtils.isEmpty(objDvi) || StringUtils.isEmpty(objDvi.getCapDvi()))
+				throw new UnsupportedOperationException("Không lấy được thông tin đơn vị");
+
+			// Add them dk loc trong child
+			Session session = entityManager.unwrap(Session.class);
+			if (!objDvi.getCapDvi().equals(Contains.CAP_TONG_CUC)) {
+				Filter filter = session.enableFilter("pFilter");
+				filter.setParameter("maDvi", objDvi.getMaDvi());
+			}
+
+			if (objDvi.getCapDvi().equals(Contains.CAP_TONG_CUC) && !StringUtils.isEmpty(objReq.getMaDvi())) {
+				Filter filter = session.enableFilter("pFilter");
+				filter.setParameter("maDvi", objReq.getMaDvi());
+			}
+
+			Map<String, String> mappingLoaiDc = new HashMap<>();
+			if (!StringUtils.isEmpty(objReq.getLoaiDchinh())
+					&& Contains.getLoaiDc(objReq.getLoaiDchinh()).equals(Contains.QD_GOC))
+				mappingLoaiDc.put(Contains.QD_GOC, "");
+			else
+				mappingLoaiDc = Contains.mappingLoaiDc;
+
+			List<String> listLoaiDc = new ArrayList<String>(mappingLoaiDc.keySet());
+
+			Page<QlnvQdMuattHdr2> dataPage = qdMuaHangHdr2Repository.selectParams(objReq.getSoQdinh(),
+					objReq.getTrangThai(), objReq.getTuNgayQdinh(), objReq.getDenNgayQdinh(), objReq.getMaHhoa(),
+					objReq.getSoQdKh(), listLoaiDc, pageable);
+
+			session.disableFilter("pFilter");
+
+			resp.setData(dataPage);
+			resp.setStatusCode(EnumResponse.RESP_SUCC.getValue());
+			resp.setMsg(EnumResponse.RESP_SUCC.getDescription());
+		} catch (Exception e) {
+			resp.setStatusCode(EnumResponse.RESP_FAIL.getValue());
+			resp.setMsg(e.getMessage());
+			log.error(e.getMessage());
+		}
+
+		return ResponseEntity.ok(resp);
+	}
+
+	@ApiOperation(value = "Lấy chi tiết thông tin quyết định mua trực tiếp dành cho cấp cục", response = List.class)
+	@PostMapping(value = PathContains.URL_CHI_TIET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<BaseResponse> detailChild(@Valid @RequestBody IdSearchReq objReq,
+			HttpServletRequest request) {
+		BaseResponse resp = new BaseResponse();
+		try {
+			if (StringUtils.isEmpty(objReq.getId()))
+				throw new UnsupportedOperationException("Không tồn tại bản ghi");
+
+			// Lay thong tin don vi quan ly
+			QlnvDmDonvi objDvi = getDvi(request);
+			if (ObjectUtils.isEmpty(objDvi) || StringUtils.isEmpty(objDvi.getCapDvi()))
+				throw new UnsupportedOperationException("Không lấy được thông tin đơn vị");
+
+			// Add them dk loc trong child
+			Session session = entityManager.unwrap(Session.class);
+			if (!objDvi.getCapDvi().equals(Contains.CAP_TONG_CUC)) {
+				Filter filter = session.enableFilter("pFilter");
+				filter.setParameter("maDvi", objDvi.getMaDvi());
+			}
+
+			if (objDvi.getCapDvi().equals(Contains.CAP_TONG_CUC) && !StringUtils.isEmpty(objReq.getMaDvi())) {
+				Filter filter = session.enableFilter("pFilter");
+				filter.setParameter("maDvi", objReq.getMaDvi());
+			}
+
+			Optional<QlnvQdMuattHdr2> qOptional = qdMuaHangHdr2Repository.findById(objReq.getId());
+
+			if (!qOptional.isPresent())
+				throw new UnsupportedOperationException("Không tồn tại bản ghi");
+
+			resp.setData(qOptional);
+			resp.setStatusCode(EnumResponse.RESP_SUCC.getValue());
+			resp.setMsg(EnumResponse.RESP_SUCC.getDescription());
+		} catch (Exception e) {
+			resp.setStatusCode(EnumResponse.RESP_FAIL.getValue());
+			resp.setMsg(e.getMessage());
+			log.error(e.getMessage());
+		}
+		return ResponseEntity.ok(resp);
+	}
 }
