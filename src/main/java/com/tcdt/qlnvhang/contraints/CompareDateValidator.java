2cdbf22909d3dd085665cf0ab81700beccc20235
bahoang84 <bahoang84@gmail.com>
2021-12-27 15:49:21 +0700

    Fixbug

--- src/main/java/com/tcdt/qlnvhang/contraints/CompareDateValidator.java
+++ src/main/java/com/tcdt/qlnvhang/contraints/CompareDateValidator.java
@@ -2,9 +2,13 @@ package com.tcdt.qlnvhang.contraints;
 
 import java.lang.reflect.Field;
 import java.util.Date;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class CompareDateValidator implements ConstraintValidator<CompareDate, Object> {
 
 	private String beforeFieldName;
@@ -27,10 +31,10 @@ public class CompareDateValidator implements ConstraintValidator<CompareDate, Ob
 
 			final Date beforeDate = (Date) beforeDateField.get(value);
 			final Date afterDate = (Date) afterDateField.get(value);
-
-			return beforeDate == null && afterDate == null || beforeDate != null && beforeDate.before(afterDate);
+			System.out.println(beforeDate.compareTo(afterDate) >= 0);
+			return beforeDate == null && afterDate == null || beforeDate != null && afterDate.compareTo(beforeDate) >= 0;
 		} catch (final Exception e) {
-			e.printStackTrace();
+			log.error(e.getMessage());
 
 			return false;
 		}
