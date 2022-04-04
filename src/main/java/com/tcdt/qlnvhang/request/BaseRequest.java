d37816da0ea2146e1d2f34242fb932f33662b302
DESKTOP-PLEIBBC\hungt <hung.ta@fruitful.io>
2022-04-04 22:21:10 +0700

    Fix paging api search

--- src/main/java/com/tcdt/qlnvhang/request/BaseRequest.java
+++ src/main/java/com/tcdt/qlnvhang/request/BaseRequest.java
@@ -4,9 +4,19 @@ import lombok.Data;
 
 @Data
 public class BaseRequest {
+
+	public static final int DEFAULT_PAGE = 0;
+	public static final int DEFAULT_LIMIT = 10;
 	PaggingReq paggingReq;
 	String trangThai;
 	String str;
 	String orderBy;
 	String orderDirection;
+
+	public PaggingReq getPaggingReq() {
+		if (this.paggingReq == null) {
+			this.paggingReq = new PaggingReq(DEFAULT_LIMIT, DEFAULT_PAGE);
+		}
+		return this.paggingReq;
+	}
 }
