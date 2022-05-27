3ddf01c5d89b006e0ef893be42c6e59510cf2310
VuNT <vunt@ttkd6.com>
2022-05-27 17:03:53 +0700

    fix dx khlcnt

--- src/main/java/com/tcdt/qlnvhang/repository/HhQdKhlcntHdrRepository.java
+++ src/main/java/com/tcdt/qlnvhang/repository/HhQdKhlcntHdrRepository.java
@@ -20,17 +20,19 @@ public interface HhQdKhlcntHdrRepository extends BaseRepository<HhQdKhlcntHdr, L
 			" AND (:loaiVthh IS NULL OR QDKHLCNT.LOAI_VTHH = :loaiVthh) "+
 			" AND (:soQd IS NULL OR QDKHLCNT.SO_QD = :soQd) "+
 			" AND (:tuNgayQd IS NULL OR QDKHLCNT.NGAY_QD >= TO_DATE(:tuNgayQd, 'yyyy-MM-dd')) "+
-			" AND (:denNgayQd IS NULL OR QDKHLCNT.NGAY_QD <= TO_DATE(:denNgayQd, 'yyyy-MM-dd')) ",
+			" AND (:denNgayQd IS NULL OR QDKHLCNT.NGAY_QD <= TO_DATE(:denNgayQd, 'yyyy-MM-dd')) "+
+			" AND (:trangThai IS NULL OR QDKHLCNT.TRANG_THAI = :trangThai) ",
 			nativeQuery = true)
-	Page<HhQdKhlcntHdr> selectPage(String namKh, String loaiVthh, String soQd, String tuNgayQd, String denNgayQd, Pageable pageable);
+	Page<HhQdKhlcntHdr> selectPage(String namKh, String loaiVthh, String soQd, String tuNgayQd, String denNgayQd,String trangThai, Pageable pageable);
 
 	@Query(value = "SELECT * FROM HH_QD_KHLCNT_HDR QDKHLCNT " +
 			" WHERE (:namKh IS NULL OR QDKHLCNT.NAM_KHOACH = TO_NUMBER(:namKh)) "+
 			" AND (:loaiVthh IS NULL OR QDKHLCNT.LOAI_VTHH = :loaiVthh) "+
 			" AND (:soQd IS NULL OR QDKHLCNT.SO_QD = :soQd) "+
 			" AND (:tuNgayQd IS NULL OR QDKHLCNT.NGAY_QD >= TO_DATE(:tuNgayQd, 'yyyy-MM-dd')) "+
-			" AND (:denNgayQd IS NULL OR QDKHLCNT.NGAY_QD <= TO_DATE(:denNgayQd, 'yyyy-MM-dd')) ",
+			" AND (:denNgayQd IS NULL OR QDKHLCNT.NGAY_QD <= TO_DATE(:denNgayQd, 'yyyy-MM-dd')) " +
+			" AND (:trangThai IS NULL OR QDKHLCNT.TRANG_THAI = :trangThai) ",
 			nativeQuery = true)
-	List<HhQdKhlcntHdr> selectAll(String namKh, String loaiVthh, String soQd, String tuNgayQd, String denNgayQd);
+	List<HhQdKhlcntHdr> selectAll(String namKh, String loaiVthh, String soQd, String tuNgayQd, String denNgayQd,String trangThai);
 
 }
