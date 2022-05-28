ebd57e201f5c14970d7df6af06f188e56f004a02
phananhtk <phananh1304@gmail.com>
2022-05-28 14:19:54 +0700

    fix bug

--- src/main/java/com/tcdt/qlnvhang/repository/quanlyphieunhapkholuongthuc/QlPhieuNhapKhoLtRepository.java
+++ src/main/java/com/tcdt/qlnvhang/repository/quanlyphieunhapkholuongthuc/QlPhieuNhapKhoLtRepository.java
@@ -12,14 +12,14 @@ import org.springframework.stereotype.Repository;
 public interface QlPhieuNhapKhoLtRepository extends BaseRepository<QlPhieuNhapKhoLt, Long>, QlPhieuNhapKhoLtRepositoryCustom {
 
     @Query(
-            value = "SELECT * FROM QL_PHIEU_NHAP_KHO_LT PNKLT\n" +
-                    "WHERE (:soPhieu IS NULL OR PNKLT.SO_PHIEU = TO_NUMBER(:soPhieu))\n" +
-                    "  AND (:ngayNhapKho IS NULL OR TO_CHAR(PNKLT.NGAY_PHE_DUYET,'YYYY-MM-DD') = :ngayNhapKho)\n" +
-                    "  AND (:maDiemKho IS NULL OR LOWER(PNKLT.MA_DIEM_KHO) = LOWER(:maDiemKho))\n" +
-                    "  AND (:maNganLo IS NULL OR LOWER(PNKLT.MA_NGAN_LO) = LOWER(:maNganLo))\n" +
-                    "  AND (:ngayTaoPhieu IS NULL OR TO_CHAR(PNKLT.NGAY_TAO,'YYYY-MM-DD') = :ngayTaoPhieu)\n" +
-                    "  AND (:thoiGianGiaoNhan IS NULL OR TO_CHAR(PNKLT.THOI_GIAN_GIAO_NHAN,'YYYY-MM-DD') = :thoiGianGiaoNhan)\n" +
-                    "  AND (:maNhaKho IS NULL OR LOWER(PNKLT.MA_NHA_KHO) = LOWER(:maNhaKho))\n" +
+            value = "SELECT * FROM QL_PHIEU_NHAP_KHO_LT PNKLT " +
+                    "WHERE (:soPhieu IS NULL OR PNKLT.SO_PHIEU = TO_NUMBER(:soPhieu))" +
+                    "  AND (:ngayNhapKho IS NULL OR TO_CHAR(PNKLT.NGAY_PHE_DUYET,'YYYY-MM-DD') = :ngayNhapKho)" +
+                    "  AND (:maDiemKho IS NULL OR LOWER(PNKLT.MA_DIEM_KHO) = LOWER(:maDiemKho))" +
+                    "  AND (:maNganLo IS NULL OR LOWER(PNKLT.MA_NGAN_LO) = LOWER(:maNganLo))" +
+                    "  AND (:ngayTaoPhieu IS NULL OR TO_CHAR(PNKLT.NGAY_TAO,'YYYY-MM-DD') = :ngayTaoPhieu)" +
+                    "  AND (:thoiGianGiaoNhan IS NULL OR TO_CHAR(PNKLT.THOI_GIAN_GIAO_NHAN,'YYYY-MM-DD') = :thoiGianGiaoNhan)" +
+                    "  AND (:maNhaKho IS NULL OR LOWER(PNKLT.MA_NHA_KHO) = LOWER(:maNhaKho))" +
                     "  AND (:nguoiGiaoHang IS NULL OR LOWER(PNKLT.NGUOI_GIAO_HANG) LIKE LOWER(CONCAT(CONCAT('%', :nguoiGiaoHang), '%')))",
             nativeQuery = true)
     Page<QlPhieuNhapKhoLt> select(Long soPhieu, String ngayNhapKho, String maDiemKho, String maNganLo, String ngayTaoPhieu, String thoiGianGiaoNhan, String maNhaKho,String nguoiGiaoHang, Pageable pageable);
