6af565e7cfd01fc76d5e4f87aa5f6b092c59c1be
buihuuduc <buihuuduc22@gmail.com>
2022-08-01 13:20:56 +0700

    search tổng hợp đề xuất kế hoạch bdg

--- src/main/java/com/tcdt/qlnvhang/response/banhangdaugia/tonghopdexuatkhbdg/BhTongHopDeXuatKhbdgSearchResponse.java
+++ src/main/java/com/tcdt/qlnvhang/response/banhangdaugia/tonghopdexuatkhbdg/BhTongHopDeXuatKhbdgSearchResponse.java
@@ -9,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Data
 @Builder
@@ -22,7 +23,21 @@ public class BhTongHopDeXuatKhbdgSearchResponse {
 	private LocalDate ngayTongHop;
 	private String noiDungTongHop;
 	private Integer namKeHoach;
-	private IdAndNameDto soQdPheDuyetKhbdg;
+	private IdAndNameDto qdPheDuyetKhbdg;
 	private String loaiHangHoa;
 	private String trangThai;
+
+	public BhTongHopDeXuatKhbdgSearchResponse(Object[] rawData) {
+		if (Objects.nonNull(rawData[0])) this.id = (Long) rawData[0];
+		if (Objects.nonNull(rawData[1])) this.maTongHop = (String) rawData[1];
+		if (Objects.nonNull(rawData[2])) this.ngayTongHop = (LocalDate) rawData[2];
+		if (Objects.nonNull(rawData[3])) this.noiDungTongHop = (String) rawData[3];
+		if (Objects.nonNull(rawData[4])) this.namKeHoach = (Integer) rawData[4];
+		if (Objects.nonNull(rawData[5])) this.trangThai = (String) rawData[5];
+
+		if (Objects.nonNull(rawData[6])) this.qdPheDuyetKhbdg.setId((Long) rawData[6]);
+		if (Objects.nonNull(rawData[7])) this.qdPheDuyetKhbdg.setName((String) rawData[7]);
+		if (Objects.nonNull(rawData[8])) this.loaiHangHoa = (String) rawData[8];
+	}
+
 }
