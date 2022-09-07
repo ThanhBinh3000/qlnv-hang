591484d4d161e91221dffedec4e7a7afb2f94858
luongnd <luongnd>
2022-09-07 23:54:55 +0700

    add bb tinh kho search

--- src/main/java/com/tcdt/qlnvhang/repository/xuathang/bbtinhkho/XhBienBanTinhKhoRepositoryCustomImpl.java
+++ src/main/java/com/tcdt/qlnvhang/repository/xuathang/bbtinhkho/XhBienBanTinhKhoRepositoryCustomImpl.java
@@ -2,29 +2,66 @@ package com.tcdt.qlnvhang.repository.xuathang.bbtinhkho;
 
 import com.tcdt.qlnvhang.entities.xuathang.bbtinhkho.XhBienBanTinhKho;
 import com.tcdt.qlnvhang.request.search.xuathang.XhBienBanTinhKhoSearchReq;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
-public class XhBienBanTinhKhoRepositoryCustomImpl implements XhBienBanTinhKhoRepositoryCustom{
+public class XhBienBanTinhKhoRepositoryCustomImpl implements XhBienBanTinhKhoRepositoryCustom {
     @PersistenceContext
     private EntityManager em;
+
     @Override
     public List<XhBienBanTinhKho> search(XhBienBanTinhKhoSearchReq req) {
 
         StringBuilder builder = new StringBuilder();
-        builder.append("SELECT * from xhBienBanTinhKho ");
-        return null;
+        builder.append("SELECT x from XhBienBanTinhKho x ");
+        //TODO: join phiếu xuất theo mã lô kho và mã vthh
+        setConditionSearch(builder, req);
+        TypedQuery<XhBienBanTinhKho> query = em.createQuery(builder.toString(), XhBienBanTinhKho.class);
+        setParameterSearch(req, query);
+        Pageable pageable = PageRequest.of(req.getPaggingReq().getPage(), req.getPaggingReq().getLimit());
+        //Set pageable
+        query.setFirstResult(pageable.getPageNumber() * pageable.getPageSize()).setMaxResults(pageable.getPageSize());
+        return query.getResultList();
     }
 
     @Override
     public int count(XhBienBanTinhKhoSearchReq req) {
         StringBuilder builder = new StringBuilder();
-        builder.append("SELECT COUNT(DISTINCT p.id) FROM xhBienBanTinhKho p ");
-
+        builder.append("SELECT COUNT(DISTINCT p.id) FROM XhBienBanTinhKho p ");
+        this.setConditionSearch(builder, req);
         TypedQuery<Long> query = em.createQuery(builder.toString(), Long.class);
+        this.setParameterSearch(req, query);
         return query.getSingleResult().intValue();
+    }
+
+    private void setConditionSearch(StringBuilder builder, XhBienBanTinhKhoSearchReq req) {
+        builder.append("WHERE 1 = 1 ");
+
+        if (!StringUtils.isEmpty(req.getQuyetDinhId())) {
+            builder.append("AND ").append("x.qdgnvnxId LIKE :qdId ");
+        }
+        if (!StringUtils.isEmpty(req.getSoBienBan())) {
+            builder.append("AND ").append("x.soBienBan LIKE :soBienBan ");
+        }
+
+        //TODO: lọc theo ngày xuất cuối
+    }
+
+    private void setParameterSearch(XhBienBanTinhKhoSearchReq req, Query query) {
+        if (!StringUtils.isEmpty(req.getQuyetDinhId())) {
+            query.setParameter(":qdId", req.getQuyetDinhId());
+        }
+        if (!StringUtils.isEmpty(req.getSoBienBan())) {
+            query.setParameter(":soBienBan", req.getSoBienBan());
+        }
+
+        //TODO: gán tham số ngày xuất cuối
     }
 }
--- src/main/java/com/tcdt/qlnvhang/request/search/xuathang/XhBienBanTinhKhoSearchReq.java
+++ src/main/java/com/tcdt/qlnvhang/request/search/xuathang/XhBienBanTinhKhoSearchReq.java
@@ -1,14 +1,30 @@
 package com.tcdt.qlnvhang.request.search.xuathang;
 
+import com.tcdt.qlnvhang.request.PaggingReq;
+import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
-public class XhBienBanTinhKhoSearchReq {
-    private String soQuyetDinh;
+@Data
+public class XhBienBanTinhKhoSearchReq{
+    public static final int DEFAULT_PAGE = 0;
+    public static final int DEFAULT_LIMIT = 10;
+
+    public static final String ORDER_BY = "id";
+
+    public static final String ORDER_TYPE = "asc";
+    private PaggingReq paggingReq;
+    private String quyetDinhId;
     private String soBienBan;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate ngayXuatTu;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate ngayXuatDen;
+    public PaggingReq getPaggingReq() {
+        if (this.paggingReq == null) {
+            this.paggingReq = new PaggingReq(DEFAULT_LIMIT, DEFAULT_PAGE,ORDER_BY,ORDER_TYPE);
+        }
+        return this.paggingReq;
+    }
 }
