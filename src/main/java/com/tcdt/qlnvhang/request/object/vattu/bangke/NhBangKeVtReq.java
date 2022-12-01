6a4bdcc9ffeae00fbfd486d2aeb6cde2f764b504
thongtv <thongtv@tecapro.com>
2022-12-01 17:57:38 +0700

    biên bản giao nhận vt bảng kê vật tư

--- src/main/java/com/tcdt/qlnvhang/request/object/vattu/bangke/NhBangKeVtReq.java
+++ src/main/java/com/tcdt/qlnvhang/request/object/vattu/bangke/NhBangKeVtReq.java
@@ -3,6 +3,7 @@ package com.tcdt.qlnvhang.request.object.vattu.bangke;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,21 +12,53 @@ import java.util.List;
 @NoArgsConstructor
 public class NhBangKeVtReq {
     private Long id;
-    private Long qdgnvnxId;
+
     private String soBangKe;
-    private Long phieuNhapKhoId;
-    private String maVatTuCha;
-    private String maVatTu;
-    private String donViTinh;
+
     private String diaChiNguoiGiao;
-    private Long hopDongId;
-    private LocalDate ngayNhap;
-    private LocalDate ngayTaoBangKe;
+
     private String trangThai;
+
     private String lyDoTuChoi;
+
+    private Long nam;
+
     private String loaiVthh;
+
     private String cloaiVthh;
-    private String moTaHangHoa;
+
+    private LocalDate ngayNhapKho;
+
+    private String soQdGiaoNvNh;
+
+    private Long idQdGiaoNvNh;
+
+    private String soHd;
+
+    private LocalDate ngayHd;
+
+    private String soPhieuNhapKho;
+
+    private Long idDdiemGiaoNvNh;
+
+    private String maDiemKho;
+
+    private String maNhaKho;
+
+    private String maNganKho;
+
+    private String maLoKho;
+
+    private String nguoiGiaoHang;
+
+    private String cmtNguoiGiaoHang;
+
+    private String donViGiaoHang;
+
+    private LocalDate thoiGianGiapNhan;
+
+    private String maDvi;
+
 
     private List<NhBangKeVtCtReq> chiTiets = new ArrayList<>();
 }
