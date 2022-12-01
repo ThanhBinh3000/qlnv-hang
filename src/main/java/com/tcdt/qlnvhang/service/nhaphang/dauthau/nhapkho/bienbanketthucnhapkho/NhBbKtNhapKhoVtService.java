884a2cb35bc2a03b7deabdfa6a998a77bf166844
VuNT <vunt@ttkd6.com>
2022-12-01 15:49:58 +0700

    ho so ky thuat

---
+++ src/main/java/com/tcdt/qlnvhang/service/nhaphang/dauthau/nhapkho/bienbanketthucnhapkho/NhBbKtNhapKhoVtService.java
@@ -0,0 +1,28 @@
+package com.tcdt.qlnvhang.service.nhaphang.dauthau.nhapkho.bienbanketthucnhapkho;
+
+import com.tcdt.qlnvhang.entities.nhaphang.dauthau.nhapkho.bienbanketthucnhapkho.NhBbKtNhapKhoVt;
+import com.tcdt.qlnvhang.request.object.vattu.bienbanketthucnhapkho.NhBbKtNhapKhoVtReq;
+import com.tcdt.qlnvhang.service.BaseService;
+
+public interface NhBbKtNhapKhoVtService extends BaseService<NhBbKtNhapKhoVt,NhBbKtNhapKhoVtReq,Long> {
+//    NhBbKtNhapKhoVtRes create(NhBbKtNhapKhoVtReq req) throws Exception;
+//
+//    NhBbKtNhapKhoVtRes update(NhBbKtNhapKhoVtReq req) throws Exception;
+//
+//    NhBbKtNhapKhoVtRes detail(Long id) throws Exception;
+//
+//    boolean delete(Long id) throws Exception;
+//
+//
+//    @Transactional(rollbackOn = Exception.class)
+//    boolean updateStatusQd(StatusReq req) throws Exception;
+//
+//    Page<NhBbKtNhapKhoVtRes> search(NhBbKtNhapKhoVtSearchReq req) throws Exception;
+//
+//    @Transactional(rollbackOn = Exception.class)
+//    boolean deleteMultiple(DeleteReq req) throws Exception;
+//
+//    boolean exportToExcel(NhBbKtNhapKhoVtSearchReq objReq, HttpServletResponse response) throws Exception;
+//
+//    BaseNhapHangCount count(Set<String> maDvis) throws Exception;
+}
