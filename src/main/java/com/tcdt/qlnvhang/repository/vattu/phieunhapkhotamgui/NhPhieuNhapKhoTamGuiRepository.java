7c88dc8cf17621466a4678dc1ee644c280659b6c
othoa <othoa>
2022-12-06 14:59:24 +0700

    fix bug search nhap kho

--- src/main/java/com/tcdt/qlnvhang/repository/vattu/phieunhapkhotamgui/NhPhieuNhapKhoTamGuiRepository.java
+++ src/main/java/com/tcdt/qlnvhang/repository/vattu/phieunhapkhotamgui/NhPhieuNhapKhoTamGuiRepository.java
@@ -3,6 +3,7 @@ package com.tcdt.qlnvhang.repository.vattu.phieunhapkhotamgui;
 import com.tcdt.qlnvhang.entities.nhaphang.dauthau.nhapkho.phieunhapkhotamgui.NhPhieuNhapKhoTamGui;
 import com.tcdt.qlnvhang.repository.BaseRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -17,6 +18,7 @@ public interface NhPhieuNhapKhoTamGuiRepository extends BaseRepository<NhPhieuNh
 
 //    Optional<NhPhieuNhapKhoTamGui> findFirstBySoPhieu(String soPhieu);
 
+    @Query(value = "select pp.* from NH_PHIEU_NHAP_KHO_TAM_GUI pp where pp.ID_QD_GIAO_NV_NH = :idDdiemGiaoNvNh and rownum =1", nativeQuery = true)
     NhPhieuNhapKhoTamGui findByIdDdiemGiaoNvNh(Long idDdiemGiaoNvNh);
 
 }
