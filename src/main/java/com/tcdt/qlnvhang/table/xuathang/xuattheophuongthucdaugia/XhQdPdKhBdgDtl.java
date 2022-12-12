45d9678bd888698d15df1ad9427170f5909ffae4
BinhTA <binhta@ttkd6.com>
2022-12-12 09:50:17 +0700

    fix

--- src/main/java/com/tcdt/qlnvhang/table/xuathang/xuattheophuongthucdaugia/XhQdPdKhBdgDtl.java
+++ src/main/java/com/tcdt/qlnvhang/table/xuathang/xuattheophuongthucdaugia/XhQdPdKhBdgDtl.java
@@ -37,6 +37,11 @@ public class XhQdPdKhBdgDtl implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Contains.FORMAT_DATE_STR)
     private Date ngayPduyet;
 
+    @Temporal(TemporalType.DATE)
+    private Date tgianDkienTu;
+    @Temporal(TemporalType.DATE)
+    private Date tgianDkienDen;
+
     private String trichYeu;
 
     private BigDecimal tongSoLuong;
