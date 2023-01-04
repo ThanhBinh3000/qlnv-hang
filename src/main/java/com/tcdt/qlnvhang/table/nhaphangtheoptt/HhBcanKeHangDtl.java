c0bef9d31372bbec587dce8e157f29fad206b568
BinhTA <binhta@ttkd6.com>
2023-01-04 17:56:13 +0700

    bảng cân kê hàng

---
+++ src/main/java/com/tcdt/qlnvhang/table/nhaphangtheoptt/HhBcanKeHangDtl.java
@@ -0,0 +1,33 @@
+package com.tcdt.qlnvhang.table.nhaphangtheoptt;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "HH_BCAN_KE_HANG_DTL")
+public class HhBcanKeHangDtl  implements Serializable {
+    private static final long serialVersionUID = 3529822360093876437L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "HH_BCAN_KE_HANG_DTL_SEQ")
+    @SequenceGenerator(sequenceName = "HH_BCAN_KE_HANG_DTL_SEQ", allocationSize = 1, name = "HH_BCAN_KE_HANG_DTL_SEQ")
+    @Column(name = "ID")
+    private Long id;
+
+    private Long idHdr;
+
+    private String maCan;
+
+    private BigDecimal soBaoBi;
+
+    private BigDecimal trongLuongCaBi;
+
+}
