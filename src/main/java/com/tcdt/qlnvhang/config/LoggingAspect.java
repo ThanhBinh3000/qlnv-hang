92e2d45c9ddcc4d5e426f7596a3a7ab68ed61da6
ta quang chien <tqchien1990@gmail.com>
2023-02-07 10:03:17 +0700

    update dx cutro

--- src/main/java/com/tcdt/qlnvhang/config/LoggingAspect.java
+++ src/main/java/com/tcdt/qlnvhang/config/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.tcdt.qlnvhang.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.tcdt.qlnvhang.entities.UserActivity;
@@ -29,54 +30,59 @@ import java.util.regex.Pattern;
 @Component
 public class LoggingAspect {
 
-    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
-    private final static String SYSTEM = "hang";
+  private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+  private final static String SYSTEM = "hang";
 
-    @Autowired
-    private Gson gson;
-    @Autowired
-    private UserActivityService userActivityService;
+  @Autowired
+  private Gson gson;
+  @Autowired
+  private UserActivityService userActivityService;
 
-    @Pointcut("within(com.tcdt.qlnvhang..*) && bean(*Controller))")
-    public void v3Controller() {
-    }
+  @Pointcut("within(com.tcdt.qlnvhang..*) && bean(*Controller))")
+  public void v3Controller() {
+  }
 
-    @Before("v3Controller()")
-    public void logBefore(JoinPoint joinPoint) {
-        try {
-            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-            String userAgent = request.getHeader("User-Agent");
-            if (ObjectUtils.isEmpty(userAgent)) {
-                userAgent = request.getHeader("user-agent");
-            }
-            Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(userAgent);
-            if (m.find()) {
-                userAgent = m.group(1);
-            }
-            if (!String.class.equals(SecurityContextHolder.getContext().getAuthentication().getPrincipal().getClass())) {
-                CustomUserDetails user = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-                UserActivity entity = new UserActivity();
-                entity.setIp(UserUtils.getClientIpAddress(request));
-                entity.setRequestMethod(request.getMethod());
-                entity.setRequestUrl(request.getRequestURI());
-                entity.setUserId(user.getUser().getId());
-                entity.setSystem(SYSTEM);
-                entity.setUserAgent(userAgent);
-                entity.setRequestBody(getBody(joinPoint.getArgs()));
-                entity.setUserName(user.getUser().getUsername());
-                Map<String, String[]> parameterMap = request.getParameterMap();
-                if (parameterMap != null && !parameterMap.isEmpty()) {
-                    entity.setRequestParameter(gson.toJson(parameterMap));
-                }
-                userActivityService.log(entity);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+  @Before("v3Controller()")
+  public void logBefore(JoinPoint joinPoint) {
+    try {
+      HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+      String userAgent = request.getHeader("User-Agent");
+      if (ObjectUtils.isEmpty(userAgent)) {
+        userAgent = request.getHeader("user-agent");
+      }
+      Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(userAgent);
+      if (m.find()) {
+        userAgent = m.group(1);
+      }
+      if (!String.class.equals(SecurityContextHolder.getContext().getAuthentication().getPrincipal().getClass())) {
+        CustomUserDetails user = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserActivity entity = new UserActivity();
+        entity.setIp(UserUtils.getClientIpAddress(request));
+        entity.setRequestMethod(request.getMethod());
+        entity.setRequestUrl(request.getRequestURI());
+        entity.setUserId(user.getUser().getId());
+        entity.setSystem(SYSTEM);
+        entity.setUserAgent(userAgent);
+        entity.setRequestBody(getBody(joinPoint.getArgs()));
+        entity.setUserName(user.getUser().getUsername());
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        if (parameterMap != null && !parameterMap.isEmpty()) {
+          entity.setRequestParameter(gson.toJson(parameterMap));
         }
+        userActivityService.log(entity);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    public String getBody(Object[] args) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(args);
+  public String getBody(Object[] args) {
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+      return mapper.writeValueAsString(args);
+    } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e);
+      return null;
     }
+  }
 }
