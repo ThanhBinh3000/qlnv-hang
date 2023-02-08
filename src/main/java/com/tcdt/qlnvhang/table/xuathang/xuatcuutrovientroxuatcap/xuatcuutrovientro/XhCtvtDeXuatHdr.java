5cdbf17258d6349a46191eeb3b5d951257aab9dd
thongtv <thongtv@tecapro.com>
2023-02-08 15:51:55 +0700

    xuất cứu trợ viện trợ: - quyết định phê duyệt phương án

--- src/main/java/com/tcdt/qlnvhang/table/xuathang/xuatcuutrovientroxuatcap/xuatcuutrovientro/XhCtvtDeXuatHdr.java
+++ src/main/java/com/tcdt/qlnvhang/table/xuathang/xuatcuutrovientroxuatcap/xuatcuutrovientro/XhCtvtDeXuatHdr.java
@@ -38,7 +38,11 @@ public class XhCtvtDeXuatHdr extends BaseEntity implements Serializable {
     private LocalDate ngayKetThuc;
     private String noiDungDx;
     private String trangThai;
+    private Long idThop;
     private String maTongHop;
+    private Long idQdPd;
+    private String soQdPd;
+    private LocalDate ngayKyQd;
     private BigDecimal tongSoLuong;
     private BigDecimal soLuongXuatCap;
     private LocalDate ngayGduyet;
