7942ef9a1d450d3105cb133cdc05c4be806fee28
BinhTA <binhta@ttkd6.com>
2023-03-15 16:12:20 +0700

    mua trục

---
+++ src/main/java/com/tcdt/qlnvhang/request/nhaphangtheoptt/hopdong/hopdongphuluc/DiaDiemGiaoNhanMttCtReq.java
@@ -0,0 +1,23 @@
+package com.tcdt.qlnvhang.request.nhaphangtheoptt.hopdong.hopdongphuluc;
+
+import lombok.Data;
+
+import javax.persistence.Transient;
+import java.math.BigDecimal;
+
+@Data
+public class DiaDiemGiaoNhanMttCtReq {
+    private Long id;
+
+    private String maDiemKho;
+
+    private String diaDiemNhap;
+
+    private Long idDiaDiem;
+
+    private BigDecimal soLuong;
+
+    private BigDecimal donGia;
+
+    private BigDecimal donGiaVat;
+}
---
+++ src/main/java/com/tcdt/qlnvhang/table/nhaphangtheoptt/hopdong/hopdongphuluc/DiaDiemGiaoNhanMttCt.java
@@ -0,0 +1,33 @@
+package com.tcdt.qlnvhang.table.nhaphangtheoptt.hopdong.hopdongphuluc;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "HH_HD_MTT_DIA_DIEM_CT")
+@Data
+public class DiaDiemGiaoNhanMttCt {
+    private static final long serialVersionUID = 1L;
+    public static final String TABLE_NAME = "HH_HD_MTT_DIA_DIEM_CT";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = TABLE_NAME + "_SEQ")
+    @SequenceGenerator(sequenceName = TABLE_NAME + "_SEQ", allocationSize = 1, name = TABLE_NAME + "_SEQ")
+    private Long id;
+
+    private String maDiemKho;
+    @Transient
+    private String tenDiemKho;
+
+    private String diaDiemNhap;
+
+    private Long idDiaDiem;
+
+    private BigDecimal soLuong;
+
+    private BigDecimal donGia;
+
+    private BigDecimal donGiaVat;
+}
