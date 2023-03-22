1fafcdc4e3553a44cb7e7e5e065b7e7bd4727f34
BinhTA <binhta@ttkd6.com>
2023-03-22 16:14:47 +0700

    bang can ke hang btt

---
+++ src/main/java/com/tcdt/qlnvhang/repository/xuathang/bantructiep/xuatkho/bangcankehang/XhBkeCanHangBttDtlRepository.java
@@ -0,0 +1,15 @@
+package com.tcdt.qlnvhang.repository.xuathang.bantructiep.xuatkho.bangcankehang;
+
+import com.tcdt.qlnvhang.entities.xuathang.bantructiep.xuatkho.bangkecanhang.XhBkeCanHangBttDtl;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface XhBkeCanHangBttDtlRepository extends JpaRepository<XhBkeCanHangBttDtl, Long> {
+
+    void deleteAllByIdHdr(Long idHdr);
+
+    List<XhBkeCanHangBttDtl> findAllByIdHdr (Long idHdr);
+}
---
+++ src/main/java/com/tcdt/qlnvhang/service/xuathang/bantructiep/xuatkho/bangkecanhang/XhBkeCanHangBttService.java
@@ -0,0 +1,10 @@
+package com.tcdt.qlnvhang.service.xuathang.bantructiep.xuatkho.bangkecanhang;
+
+import com.tcdt.qlnvhang.entities.xuathang.bantructiep.xuatkho.bangkecanhang.XhBkeCanHangBttHdr;
+import com.tcdt.qlnvhang.request.xuathang.bantructiep.xuatkho.bangkecanhang.XhBkeCanHangBttHdrReq;
+import com.tcdt.qlnvhang.service.BaseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface XhBkeCanHangBttService extends BaseService<XhBkeCanHangBttHdr, XhBkeCanHangBttHdrReq, Long> {
+}
