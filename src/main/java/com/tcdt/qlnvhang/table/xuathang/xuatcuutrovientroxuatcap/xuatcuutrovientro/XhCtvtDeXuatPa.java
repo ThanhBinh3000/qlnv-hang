1a501ba9513c555bc7feec5bf0342358ef0b554b
ta quang chien <tqchien1990@gmail.com>
2023-04-04 15:47:38 +0700

    fix phan quyen du lieu ctvt de xuat

--- src/main/java/com/tcdt/qlnvhang/table/xuathang/xuatcuutrovientroxuatcap/xuatcuutrovientro/XhCtvtDeXuatPa.java
+++ src/main/java/com/tcdt/qlnvhang/table/xuathang/xuatcuutrovientroxuatcap/xuatcuutrovientro/XhCtvtDeXuatPa.java
@@ -15,39 +15,39 @@ import java.math.BigDecimal;
 @Setter
 public class XhCtvtDeXuatPa implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    public static final String TABLE_NAME = "XH_CTVT_DE_XUAT_PA";
+  private static final long serialVersionUID = 1L;
+  public static final String TABLE_NAME = "XH_CTVT_DE_XUAT_PA";
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = XhCtvtDeXuatPa.TABLE_NAME + "_SEQ")
-    @SequenceGenerator(sequenceName = XhCtvtDeXuatPa.TABLE_NAME + "_SEQ", allocationSize = 1, name = XhCtvtDeXuatPa.TABLE_NAME + "_SEQ")
-    private Long id;
-    private Long idHdr;
-    private String noiDung;
-    private BigDecimal soLuongXuat;
-    private String maDviCuc;
-    private BigDecimal tonKhoCuc;
-    private BigDecimal soLuongXuatCuc;
-    private String maDviChiCuc;
-    private BigDecimal tonKhoChiCuc;
-    private BigDecimal tonKhoCloaiVthh;
-    private String loaiVthh;
-    private String cloaiVthh;
-    private BigDecimal soLuongXuatChiCuc;
-    private String donViTinh;
-    private BigDecimal donGiaKhongVat;
-    private BigDecimal thanhTien;
-    private BigDecimal soLuongXuatCap;
-    @Transient
-    private String tenLoaiVthh;
-    @Transient
-    private String tenCloaiVthh;
-    @Transient
-    private String tenCuc;
-    @Transient
-    private String tenChiCuc;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "idHdr", nullable = false)
-    @JsonIgnore
-    private XhCtvtDeXuatHdr xhCtvtDeXuatHdr;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = XhCtvtDeXuatPa.TABLE_NAME + "_SEQ")
+  @SequenceGenerator(sequenceName = XhCtvtDeXuatPa.TABLE_NAME + "_SEQ", allocationSize = 1, name = XhCtvtDeXuatPa.TABLE_NAME + "_SEQ")
+  private Long id;
+  private Long idHdr;
+  private String noiDung;
+  private BigDecimal soLuongXuat;
+  private String maDviCuc;
+  private BigDecimal tonKhoCuc;
+  private BigDecimal soLuongXuatCuc;
+  private String maDviChiCuc;
+  private BigDecimal tonKhoChiCuc;
+  private BigDecimal tonKhoCloaiVthh;
+  private String loaiVthh;
+  private String cloaiVthh;
+  private BigDecimal soLuongXuatChiCuc;
+  private String donViTinh;
+  private BigDecimal donGiaKhongVat;
+  private BigDecimal thanhTien;
+  private BigDecimal soLuongXuatCap;
+  @Transient
+  private String tenLoaiVthh;
+  @Transient
+  private String tenCloaiVthh;
+  @Transient
+  private String tenCuc;
+  @Transient
+  private String tenChiCuc;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "idHdr", insertable = false, updatable = false)
+  @JsonIgnore
+  private XhCtvtDeXuatHdr xhCtvtDeXuatHdr;
 }
