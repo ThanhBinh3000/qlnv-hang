1801a86a634a4582c31c8a4005d60d95fe00e561
thongtv <thongtv@tecapro.com>
2023-04-17 11:02:01 +0700

    fix bug : update trạng thái QĐGiao NvXh

--- src/main/java/com/tcdt/qlnvhang/request/xuathang/xuatcuutrovientroxuatcap/xuatcuutrovientro/SearchXhCtvtBbTinhKho.java
+++ src/main/java/com/tcdt/qlnvhang/request/xuathang/xuatcuutrovientroxuatcap/xuatcuutrovientro/SearchXhCtvtBbTinhKho.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 public class SearchXhCtvtBbTinhKho  extends BaseRequest {
   private String maDvi;
   private String dvql;
-  private Long nam;
+  private Integer nam;
   private String soQdGiaoNvXh;
   private String soBbTinhKho;
   private LocalDate ngayBatDauXuatTu;
