5c77f6feb0626cf55f058bf1e2a6f74203f42f4f
GiangNT <giangnt@tecapro.com.vn>
2023-05-05 16:32:14 +0700

    THEM API DANH SACH QD

--- src/main/java/com/tcdt/qlnvhang/controller/dieuchuyennoibo/DcnbQuyetDinhDcTcHdrController.java
+++ src/main/java/com/tcdt/qlnvhang/controller/dieuchuyennoibo/DcnbQuyetDinhDcTcHdrController.java
@@ -187,4 +187,21 @@ public class DcnbQuyetDinhDcTcHdrController extends BaseController {
 
     }
   }
+
+  @ApiOperation(value = "Danh sách quyết định", response = List.class)
+  @PostMapping(value = "/danh-sach-quyet-dinh", produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<BaseResponse> danhSachQuyetDinh(@CurrentUser CustomUserDetails currentUser,@RequestBody SearchDcnbQuyetDinhDcTc objReq) {
+    BaseResponse resp = new BaseResponse();
+    try {
+      resp.setData(dcnbQuyetDinhDcTcDtlService.danhSachQuyetDinh(currentUser,objReq));
+      resp.setStatusCode(EnumResponse.RESP_SUCC.getValue());
+      resp.setMsg(EnumResponse.RESP_SUCC.getDescription());
+    } catch (Exception e) {
+      resp.setStatusCode(EnumResponse.RESP_FAIL.getValue());
+      resp.setMsg(e.getMessage());
+      log.error("Tra cứu thông tin : {}", e);
+    }
+    return ResponseEntity.ok(resp);
+  }
 }
