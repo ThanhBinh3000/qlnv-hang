2b64f1bb757b8893eb36a9f7961ea99924056968
hoangld <hoang1312ng@gmail.com>
2023-05-06 17:08:10 +0700

    Chỉnh sửa thdcnb

--- src/main/java/com/tcdt/qlnvhang/repository/dieuchuyennoibo/THKeHoachDieuChuyenCucHdrRepository.java
+++ src/main/java/com/tcdt/qlnvhang/repository/dieuchuyennoibo/THKeHoachDieuChuyenCucHdrRepository.java
@@ -41,13 +41,13 @@ public interface THKeHoachDieuChuyenCucHdrRepository extends JpaRepository<THKeH
         "AND h.ngaytao <= ?6")
     List<THKeHoachDieuChuyenCucHdr> findByDonViAndTrangThaiTongCuc(String maDVi,String trangThai,String loaiDieuChuyen, String loaiHangHoa, String chungLoaiHangHoa, LocalDate thoiGianTongHop);
 
-    @Query(nativeQuery = true,value = "SELECT distinct h FROM THKeHoachDieuChuyenCucHdr h \n" +
-            "LEFT JOIN THKeHoachDieuChuyenCucKhacCucDtl dtl ON dtl.hdrId = h.id \n" +
-            "LEFT JOIN DcnbKeHoachDcHdr khhdr ON CAST(khhdr.id AS string) = dtl.dcnbKeHoachDcHdrId\n" +
-            "LEFT JOIN DcnbKeHoachDcDtl khdtl ON khdtl.hdrId = khhdr.id \n " +
-            "WHERE h.maDvi = ?1 AND h.trangThai = ?2 AND h.loaiDieuChuyen = ?3 \n" +
-            "AND (?4 IS NULL OR khdtl.loaiVthh = ?4) \n" +
-            "AND (?5 IS NULL OR khdtl.cloaiVthh = ?5)\n" +
+    @Query(nativeQuery = true,value = "SELECT distinct h FROM THKeHoachDieuChuyenCucHdr h " +
+            "LEFT JOIN THKeHoachDieuChuyenCucKhacCucDtl dtl ON dtl.hdrId = h.id " +
+            "LEFT JOIN DcnbKeHoachDcHdr khhdr ON CAST(khhdr.id AS string) = dtl.dcnbKeHoachDcHdrId" +
+            "LEFT JOIN DcnbKeHoachDcDtl khdtl ON khdtl.hdrId = khhdr.id  " +
+            "WHERE h.maDvi = ?1 AND h.trangThai = ?2 AND h.loaiDieuChuyen = ?3 " +
+            "AND (?4 IS NULL OR khdtl.loaiVthh = ?4) " +
+            "AND (?5 IS NULL OR khdtl.cloaiVthh = ?5)" +
             "AND h.ngaytao <= ?6")
     List<THKeHoachDieuChuyenCucHdr> findByDonViAndTrangThaiTongCucKhacCuc(String maDVi,String trangThai,String loaiDieuChuyen, String loaiHangHoa, String chungLoaiHangHoa, LocalDate thoiGianTongHop);
     @Query(nativeQuery = true,value = "SELECT h.ID FROM DCNB_TH_KE_HOACH_DCC_HDR h WHERE h.MA_DVI = ?1 AND h.LOAI_DC = ?2")
