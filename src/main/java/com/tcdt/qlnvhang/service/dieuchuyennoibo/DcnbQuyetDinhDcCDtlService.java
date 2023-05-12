e88be63b342619133383f84738487d9ba6b7899d
GiangNT <giangnt@tecapro.com.vn>
2023-05-12 11:04:36 +0700

    fix bug

--- src/main/java/com/tcdt/qlnvhang/service/dieuchuyennoibo/DcnbQuyetDinhDcCDtlService.java
+++ src/main/java/com/tcdt/qlnvhang/service/dieuchuyennoibo/DcnbQuyetDinhDcCDtlService.java
@@ -229,7 +229,7 @@ public class DcnbQuyetDinhDcCDtlService extends BaseServiceImpl {
             List<FileDinhKem> canCu = fileDinhKemService.search(data.getId(), Arrays.asList(DcnbQuyetDinhDcCHdr.TABLE_NAME + "_CAN_CU"));
             data.setCanCu(canCu);
             List<FileDinhKem> quyetDinh = fileDinhKemService.search(data.getId(), Arrays.asList(DcnbQuyetDinhDcCHdr.TABLE_NAME + "_QUYET_DINH"));
-            data.setCanCu(quyetDinh);
+            data.setQuyetDinh(quyetDinh);
 
             List<DcnbQuyetDinhDcCDtl> sachQuyetDinh = data.getDanhSachQuyetDinh();
             sachQuyetDinh.forEach(data1 -> {
