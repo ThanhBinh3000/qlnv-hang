71bcbac6fbb52015123cc48a3f63d7607dc57667
hoangld <hoang1312ng@gmail.com>
2023-05-22 18:28:28 +0700

    Biên bản tịnh kho

---
+++ src/main/java/com/tcdt/qlnvhang/table/dieuchuyennoibo/DcnbBienBanTinhKhoDtl.java
@@ -0,0 +1,25 @@
+package com.tcdt.qlnvhang.table.dieuchuyennoibo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = DcnbBienBanLayMauHdr.TABLE_NAME)
+@Getter
+@Setter
+public class DcnbBienBanTinhKhoDtl {
+    private static final long serialVersionUID = 1L;
+    public static final String TABLE_NAME = "DCNB_BIEN_BAN_TINH_KHO_HDR";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = DcnbBienBanLayMauDtl.TABLE_NAME + "_SEQ")
+    @SequenceGenerator(sequenceName = DcnbBienBanLayMauDtl.TABLE_NAME + "_SEQ", allocationSize = 1, name = DcnbBienBanLayMauDtl.TABLE_NAME + "_SEQ")
+    private Long id;
+
+    @Column(name = "HDR_ID")
+    private Long hdrId;
+
+
+}
