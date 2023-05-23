15a41ce19b252a90328f72959ee23300344bfcef
thongtv <thongtv@tecapro.com>
2023-05-23 11:55:48 +0700

    xuất tiêu hủy: quyết định tiêu hủy

---
+++ src/main/java/com/tcdt/qlnvhang/repository/xuathang/thanhlytieuhuy/tieuhuy/XhThQuyetDinhRepository.java
@@ -0,0 +1,37 @@
+package com.tcdt.qlnvhang.repository.xuathang.thanhlytieuhuy.tieuhuy;
+
+import com.tcdt.qlnvhang.request.xuathang.thanhlytieuhuy.tieuhuy.SearchXhThQuyetDinh;
+import com.tcdt.qlnvhang.table.xuathang.thanhlytieuhuy.tieuhuy.XhThQuyetDinhHdr;
+import feign.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface XhThQuyetDinhRepository extends JpaRepository<XhThQuyetDinhHdr,Long> {
+
+  @Query("SELECT DISTINCT  c FROM XhThQuyetDinhHdr c " +
+      " WHERE 1=1 " +
+      "AND (:#{#param.dvql} IS NULL OR c.maDvi LIKE CONCAT(:#{#param.dvql},'%')) " +
+      "AND (:#{#param.nam} IS NULL OR c.nam = :#{#param.nam}) " +
+      "AND (:#{#param.soQd} IS NULL OR LOWER(c.soQd) LIKE CONCAT('%',LOWER(:#{#param.soQd}),'%')) " +
+      "AND (:#{#param.soHoSo} IS NULL OR LOWER(c.soHoSo) LIKE CONCAT('%',LOWER(:#{#param.soHoSo}),'%')) " +
+      "AND ((:#{#param.ngayKyTu}  IS NULL OR c.ngayKy >= :#{#param.ngayKyTu})" +
+      "AND (:#{#param.ngayKyDen}  IS NULL OR c.ngayKy <= :#{#param.ngayKyDen}) ) " +
+      "AND (:#{#param.trangThai} IS NULL OR c.trangThai = :#{#param.trangThai}) " +
+      "ORDER BY c.ngaySua desc , c.ngayTao desc, c.id desc"
+  )
+  Page<XhThQuyetDinhHdr> search (@Param("param") SearchXhThQuyetDinh param, Pageable pageable);
+
+
+  void deleteAllByIdIn(List<Long> listId);
+
+  List<XhThQuyetDinhHdr> findByIdIn(List<Long> ids);
+
+  List<XhThQuyetDinhHdr> findAllByIdIn(List<Long> listId);
+
+  Optional<XhThQuyetDinhHdr> findBySoQd(String soQd);
+}
