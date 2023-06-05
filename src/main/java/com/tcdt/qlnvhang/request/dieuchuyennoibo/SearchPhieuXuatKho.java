e6337b4de49c7f13bda1e1b7b6a5402ab0007ec3
VuNT <cuongnv@ttkd6.com>
2023-06-05 09:00:15 +0700

    fix phieu xuat kho

---
+++ src/main/java/com/tcdt/qlnvhang/request/dieuchuyennoibo/SearchPhieuXuatKho.java
@@ -0,0 +1,21 @@
+package com.tcdt.qlnvhang.request.dieuchuyennoibo;
+
+import com.tcdt.qlnvhang.request.BaseRequest;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class SearchPhieuXuatKho extends BaseRequest {
+    private Integer nam;
+    private String soQdinhDcc;
+    private String soBbLayMau;
+    private String dViKiemNghiem;
+    private LocalDate tuNgay;
+    private LocalDate denNgay;
+    private String maDvi;
+    private String loaiDc;
+    private String trangThai;
+}
