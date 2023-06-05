c7fc4d51ea9e94dcd990ca26ad4325ee93a3a720
VuxNT <vunt1809@gmail.com>
2023-06-05 14:23:21 +0700

    fix xh phieu knghiem cl

---
+++ src/main/java/com/tcdt/qlnvhang/repository/dieuchuyennoibo/DcnbBBNTBQDtlRepository.java
@@ -0,0 +1,17 @@
+package com.tcdt.qlnvhang.repository.dieuchuyennoibo;
+
+import com.tcdt.qlnvhang.table.dieuchuyennoibo.DcnbBBNTBQDtl;
+import com.tcdt.qlnvhang.table.dieuchuyennoibo.DcnbPhieuXuatKhoDtl;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DcnbBBNTBQDtlRepository extends JpaRepository<DcnbBBNTBQDtl, Long> {
+
+//    List<DcnbPhieuXuatKhoDtl> findAllByHdrId(Long hdrId);
+//
+//    void deleteByHdrId(Long hdrId);
+
+}
