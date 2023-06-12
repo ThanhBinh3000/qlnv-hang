9c12686dc2c426512a3c7e8f22c4e068654ae96d
hoangld <hoang1312ng@gmail.com>
2023-06-12 15:38:40 +0700

    fix bug

---
+++ src/main/java/com/tcdt/qlnvhang/request/dieuchuyennoibo/DcnbBienBanHaoDoiHdrReq.java
@@ -0,0 +1,115 @@
+package com.tcdt.qlnvhang.request.dieuchuyennoibo;
+
+import com.tcdt.qlnvhang.table.dieuchuyennoibo.DcnbBienBanHaoDoiTtDtl;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class DcnbBienBanHaoDoiHdrReq {
+    private Long id;
+
+    private String loaiDc;
+
+    private Boolean thayDoiThuKho;
+
+    private String loaiVthh;
+
+    private String tenLoaiVthh;
+
+    private String cloaiVthh;
+
+    private String tenCloaiVthh;
+
+    private Integer nam;
+
+    private String maDvi;
+
+    private String tenDvi;
+
+    private String maQhns;
+
+    private String soBienBan;
+
+    private LocalDate ngayLap;
+
+    private String soQdinhDcc;
+
+    private Long qDinhDccId;
+
+    private LocalDate ngayKyQdDcc;
+
+    private String maDiemKho;
+
+    private String tenDiemKho;
+
+    private String maNhaKho;
+
+    private String tenNhaKho;
+
+    private String maNganKho;
+
+    private String tenNganKho;
+
+    private String maLoKho;
+
+    private String tenLoKho;
+
+    private String soBbTinhKho;
+
+    private Long bBTinhKhoId;
+
+    private Double tongSlXuatTheoQd;
+
+    private LocalDate ngayKetThucXuatQd;
+
+    private Double tongSlXuatTheoTt;
+
+    private LocalDate ngayKetThucXuatTt;
+
+    private Double slHaoTt;
+
+    private Double tiLeHaoTt;
+
+    private Double slHaoVuotDm;
+
+    private Double tiLeHaoVuotDm;
+
+    private String nguyenNhan;
+
+    private String kienNghi;
+
+    private String ghiChu;
+
+    private String thuKho;
+
+    private LocalDate ngayPduyetKtvBQ;
+
+    private String ktvBaoQuan;
+
+    private Long ktvBaoQuanId;
+
+    private LocalDate ngayPduyetKt;
+
+    private String keToan;
+
+    private Long keToanId;
+
+    private LocalDate ngayPduyetLdcc;
+
+    private String lanhDaoChiCuc;
+
+    private Long lanhDaoChiCucId;
+
+    private String trangThai;
+
+    private LocalDate ngayGduyet;
+
+    private Long nguoiGduyetId;
+
+    private String lyDoTuChoi;
+
+    private List<DcnbBienBanHaoDoiTtDtl> dcnbBienBanHaoDoiTtDtl = new ArrayList<>();
+}
---
+++ src/main/java/com/tcdt/qlnvhang/response/dieuChuyenNoiBo/DcnbBienBanHaoDoiHdrDTO.java
@@ -0,0 +1,40 @@
+package com.tcdt.qlnvhang.response.dieuChuyenNoiBo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class DcnbBienBanHaoDoiHdrDTO {
+    private Long id;
+    private Long bangKeCanHangId;
+    private Long bbTinhKhoId;
+    private Long qDinhDcId;
+    private Long phieuXuatKhoId;
+    private String soQdinh;
+    private Integer nam;
+    private LocalDate ngayHieuLuc;
+    private String maDiemKho;
+    private String tenDiemKho;
+    private String maLoKho;
+    private String tenLoKho;
+    private String soBienBanHaoDoi;
+    private String soPhieuXuatKho;
+    private String soBangKeXuatDcLt;
+    private LocalDate ngayXuatKho;
+    private String trangThai;
+    private String tenTrangThai;
+    private String maHangHoa;
+    private String tenHangHoa;
+    private String maChLoaiHangHoa;
+    private String tenChLoaiHangHoa;
+    private String maNhaKho;
+    private String tenNhaKho;
+    private String donViTinh;
+    private String tenDonViTinh;
+    private String maNganKho;
+    private String tenNganKho;
+    private LocalDate ngayKyQDinh;
+}
