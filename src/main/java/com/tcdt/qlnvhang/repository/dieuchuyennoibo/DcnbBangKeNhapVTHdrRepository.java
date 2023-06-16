68178a873eda95a28929de78534e229098ecb398
GiangNT <giangnt@tecapro.com.vn>
2023-06-16 14:54:20 +0700

    fix bug bang kê nhập vật tư

--- src/main/java/com/tcdt/qlnvhang/repository/dieuchuyennoibo/DcnbBangKeNhapVTHdrRepository.java
+++ src/main/java/com/tcdt/qlnvhang/repository/dieuchuyennoibo/DcnbBangKeNhapVTHdrRepository.java
@@ -1,9 +1,62 @@
 package com.tcdt.qlnvhang.repository.dieuchuyennoibo;
 
+import com.tcdt.qlnvhang.request.dieuchuyennoibo.DcnbBangKeNhapVTReq;
+import com.tcdt.qlnvhang.response.dieuChuyenNoiBo.DcnbBangKeNhapVTHdrDTO;
+import com.tcdt.qlnvhang.table.dieuchuyennoibo.DcnbBangKeNhapVTDtl;
 import com.tcdt.qlnvhang.table.dieuchuyennoibo.DcnbBangKeNhapVTHdr;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DcnbBangKeNhapVTHdrRepository extends JpaRepository<DcnbBangKeNhapVTHdr, Long> {
+    Optional<DcnbBangKeNhapVTHdr> findFirstBySoBangKe(String soBangKe);
+
+    List<DcnbBangKeNhapVTDtl> findAllByIdIn(List<Long> listMulti);
+    @Query(value = "SELECT new com.tcdt.qlnvhang.response.dieuChuyenNoiBo.DcnbBangKeNhapVTHdrDTO(" +
+            "bknvt.id,qdc.id,qdc.soQdinh,qdc.nam,khdcd.maDiemKho,khdcd.tenDiemKho,khdcd.maNhaKho, khdcd.tenNhaKho,khdcd.maNganKho, khdcd.tenNganKho, khdcd.maLoKho," +
+            "khdcd.tenLoKho,bblm.id,bblm.soBbLayMau, bknvt.soBangKe, bknvt.soBangKe,pnk.soPhieuNhapKho, pnk.id, pnk.ngayLap,bknvt.trangThai ,bknvt.trangThai) " +
+            "FROM DcnbQuyetDinhDcCHdr qdc " +
+            "LEFT JOIN DcnbBangKeNhapVTHdr bknvt ON bknvt.qDinhDccId = qdc.id "+
+            "LEFT JOIN DcnbBienBanLayMauHdr bblm ON bblm.qDinhDccId = qdc.id "+
+            "LEFT JOIN DcnbPhieuNhapKhoHdr pnk ON pnk.qdDcCucId = qdc.id " +
+            "LEFT JOIN DcnbQuyetDinhDcCDtl qdcd On qdcd.hdrId = qdc.id " +
+            "LEFT JOIN DcnbKeHoachDcHdr khdch On khdch.id = qdcd.keHoachDcHdrId " +
+            "LEFT JOIN DcnbKeHoachDcDtl khdcd On khdcd.hdrId = khdch.id " +
+            "WHERE 1 =1 "+
+            "AND qdc.trangThai = '29' AND qdc.loaiDc = :#{#param.loaiDc} AND (:#{#param.type} IS NULL OR bknvt.type = :#{#param.type})"+
+            "AND ((:#{#param.loaiQdinh} IS NULL OR qdc.loaiQdinh = :#{#param.loaiQdinh})) "+
+            "AND ((:#{#param.maDvi} IS NULL OR qdc.maDvi = :#{#param.maDvi}) OR (:#{#param.maDvi} IS NULL OR qdc.maDvi = :#{#param.maDvi}))"+
+            "AND (:#{#param.nam} IS NULL OR qdc.nam = :#{#param.nam}) " +
+            "AND (:#{#param.soBangKe} IS NULL OR LOWER(bknvt.soBangKe) LIKE CONCAT('%',LOWER(:#{#param.soBangKe}),'%')) " +
+            "AND (:#{#param.soQdinhDcc} IS NULL OR LOWER(bknvt.soQdinhDcc) LIKE CONCAT('%',LOWER(:#{#param.soQdinhDcc}),'%')) " +
+            "ORDER BY bknvt.soQdinhDcc desc, bknvt.nam desc")
+    Page<DcnbBangKeNhapVTHdrDTO> searchPage(@Param("param")DcnbBangKeNhapVTReq req, Pageable pageable);
+    @Query(value = "SELECT new com.tcdt.qlnvhang.response.dieuChuyenNoiBo.DcnbBangKeNhapVTHdrDTO(" +
+            "bknvt.id,qdc.id,qdc.soQdinh,qdc.nam,khdcd.maDiemKho,khdcd.tenDiemKho,khdcd.maNhaKho, khdcd.tenNhaKho,khdcd.maNganKho, khdcd.tenNganKho, khdcd.maLoKho," +
+            "khdcd.tenLoKho,bblm.id,bblm.soBbLayMau, bknvt.soBangKe, bknvt.soBangKe,pnk.soPhieuNhapKho, pnk.id, pnk.ngayLap,bknvt.trangThai ,bknvt.trangThai) " +
+            "FROM DcnbQuyetDinhDcCHdr qdc " +
+            "LEFT JOIN DcnbDataLinkHdr dtlh On dtlh.qdCcParentId = qdc.id " +
+            "LEFT JOIN DcnbBangKeNhapVTHdr bknvt ON bknvt.qDinhDccId = dtlh.qdCcId "+
+            "LEFT JOIN DcnbBienBanLayMauHdr bblm ON bblm.qDinhDccId = dtlh.qdCcId "+
+            "LEFT JOIN DcnbPhieuNhapKhoHdr pnk ON pnk.qdDcCucId = dtlh.qdCcId " +
+            "LEFT JOIN DcnbQuyetDinhDcCDtl qdcd On qdcd.hdrId = dtlh.qdCcId " +
+            "LEFT JOIN DcnbKeHoachDcHdr khdch On khdch.id = qdcd.keHoachDcHdrId " +
+            "LEFT JOIN DcnbKeHoachDcDtl khdcd On khdcd.hdrId = khdch.id " +
+            "WHERE 1 =1 "+
+            "AND qdc.trangThai = '29' AND qdc.loaiDc = :#{#param.loaiDc} AND (:#{#param.type} IS NULL OR bknvt.type = :#{#param.type})"+
+            "AND ((:#{#param.loaiQdinh} IS NULL OR qdc.loaiQdinh = :#{#param.loaiQdinh})) "+
+            "AND ((:#{#param.maDvi} IS NULL OR qdc.maDvi = :#{#param.maDvi}) OR (:#{#param.maDvi} IS NULL OR qdc.maDvi = :#{#param.maDvi}))"+
+            "AND (:#{#param.nam} IS NULL OR qdc.nam = :#{#param.nam}) " +
+            "AND (:#{#param.soBangKe} IS NULL OR LOWER(bknvt.soBangKe) LIKE CONCAT('%',LOWER(:#{#param.soBangKe}),'%')) " +
+            "AND (:#{#param.soQdinhDcc} IS NULL OR LOWER(bknvt.soQdinhDcc) LIKE CONCAT('%',LOWER(:#{#param.soQdinhDcc}),'%')) " +
+            "ORDER BY bknvt.soQdinhDcc desc, bknvt.nam desc")
+    Page<DcnbBangKeNhapVTHdrDTO> searchPageCuc(@Param("param")DcnbBangKeNhapVTReq req, Pageable pageable);
 }
--- src/main/java/com/tcdt/qlnvhang/request/dieuchuyennoibo/DcnbBangKeNhapVTReq.java
+++ src/main/java/com/tcdt/qlnvhang/request/dieuchuyennoibo/DcnbBangKeNhapVTReq.java
@@ -1,9 +1,68 @@
 package com.tcdt.qlnvhang.request.dieuchuyennoibo;
 
+import com.tcdt.qlnvhang.request.BaseRequest;
+import com.tcdt.qlnvhang.table.dieuchuyennoibo.DcnbBangKeNhapVTDtl;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
-public class DcnbBangKeNhapVTReq {
+public class DcnbBangKeNhapVTReq extends BaseRequest {
+    private Long id;
+    private String loaiDc;
+    private String loaiVthh;
+    private String cloaiVthh;
+    private Integer nam;
+    private String loaiQdinh;
+    private String soBangKe;
+    private LocalDate ngayNhap;
+    private String maDvi;
+    private Long qhnsId;
+    private String maQhns;
+    private Long qDinhDccId;
+    private String soQdinhDcc;
+    private LocalDate ngayKyQdinhDcc;
+    private String soHopDong;
+    private Long phieuXuatKhoId;
+    private String soPhieuXuatKho;
+    private String maDiemKho;
+    private String tenDiemKho;
+    private String diaDaDiemKho;
+    private String maNhaKho;
+    private String tenNhaKho;
+    private String maNganKho;
+    private String tenNganKho;
+    private String maLoKho;
+    private String tenLoKho;
+    private String maLanhDaoChiCuc;
+    private String tenLanhDaoChiCuc;
+    private Long thuKhoId;
+    private String tenThuKho;
+    private Long phuTrachId;
+    private String tenPhuTrach;
+    private String tenNguoiGiaoHang;
+    private String cccd;
+    private String donViNguoiGiaoHang;
+    private String diaChiDonViNguoiGiaoHang;
+    private LocalDate thoiHanGiaoNhan;
+    private String donViTinh;
+    private String trangThai;
+    private String lyDoTuChoi;
+    private Long nguoiGDuyet;
+    private LocalDate ngayGDuyet;
+    private Long nguoiPDuyetTvqt;
+    private LocalDate ngayPDuyetTvqt;
+    private Long nguoiPDuyet;
+    private LocalDate ngayPDuyet;
+    private String type;
+    private String typeDataLink;
+    private List<DcnbBangKeNhapVTDtl> dcnbbangkenhapvtdtl = new ArrayList<>();
 }
