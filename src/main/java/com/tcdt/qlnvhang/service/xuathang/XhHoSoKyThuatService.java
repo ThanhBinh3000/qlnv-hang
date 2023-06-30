e2549a92dc5a3b34719b2cfbaab770fb4b4b408b
ta quang chien <tqchien1990@gmail.com>
2023-06-30 10:57:08 +0700

    update ho so ky thuat

--- src/main/java/com/tcdt/qlnvhang/service/xuathang/XhHoSoKyThuatService.java
+++ src/main/java/com/tcdt/qlnvhang/service/xuathang/XhHoSoKyThuatService.java
@@ -7,6 +7,7 @@ import com.tcdt.qlnvhang.entities.nhaphang.dauthau.kiemtracl.hosokythuat.NhHoSoK
 import com.tcdt.qlnvhang.entities.nhaphang.dauthau.kiemtracl.hosokythuat.NhHoSoKyThuatCt;
 import com.tcdt.qlnvhang.enums.TrangThaiAllEnum;
 import com.tcdt.qlnvhang.jwt.CustomUserDetails;
+import com.tcdt.qlnvhang.repository.bbanlaymau.BienBanLayMauRepository;
 import com.tcdt.qlnvhang.repository.kiemtrachatluong.NhHoSoBienBanRepository;
 import com.tcdt.qlnvhang.repository.vattu.hosokythuat.NhHoSoKyThuatRepository;
 import com.tcdt.qlnvhang.repository.xuathang.XhHoSoKyThuatRepository;
@@ -45,6 +46,8 @@ public class XhHoSoKyThuatService extends BaseServiceImpl {
   private FileDinhKemService fileDinhKemService;
   @Autowired
   private NhHoSoKyThuatService nhHoSoKyThuatService;
+  @Autowired
+  private BienBanLayMauRepository bienBanLayMauRepository;
 
   public Page<NhHoSoKyThuatDTO> searchPage(CustomUserDetails currentUser, SearchHoSoKyThuatReq req) throws Exception {
 
@@ -86,12 +89,12 @@ public class XhHoSoKyThuatService extends BaseServiceImpl {
     if (currentUser == null) {
       throw new Exception("Bad request.");
     }
-    if (!DataUtils.isNullObject(objReq.getSoHs())) {
+    /*if (!DataUtils.isNullObject(objReq.getSoHs())) {
       Optional<XhHoSoKyThuatHdr> optional = xhHoSoKyThuatRepository.findBySoHs(objReq.getSoHs());
       if (optional.isPresent()) {
         throw new Exception("số hồ sơ đã tồn tại");
       }
-    }
+    }*/
     XhHoSoKyThuatHdr data = new XhHoSoKyThuatHdr();
     BeanUtils.copyProperties(objReq, data);
     data.setMaDvi(currentUser.getUser().getDepartment());
@@ -253,7 +256,10 @@ public class XhHoSoKyThuatService extends BaseServiceImpl {
         }
 
         xhHskt = new XhHoSoKyThuatHdr();
-        xhHskt.setIdHsktNh(objReq.getId());
+
+        xhHskt.setIdHskt(objReq.getId());
+        xhHskt.setSoHskt(nhHoSoKyThuat.getSoHoSoKyThuat());
+        xhHskt.setSoBbLayMauNh(nhHoSoKyThuat.getSoBbLayMau());
         xhHskt.setXhHoSoKyThuatDtl(listDtl);
         xhHskt.setMapDmucDvi(mapDmucDvi);
         xhHskt.setMapVthh(mapVthh);
@@ -270,6 +276,7 @@ public class XhHoSoKyThuatService extends BaseServiceImpl {
           s1.setFileDinhKem(fileDinhKem);
         });
       });
+
       xhHskt.setMapDmucDvi(mapDmucDvi);
       xhHskt.setMapVthh(mapVthh);
     }
--- src/main/java/com/tcdt/qlnvhang/table/xuathang/hosokythuat/XhHoSoKyThuatHdr.java
+++ src/main/java/com/tcdt/qlnvhang/table/xuathang/hosokythuat/XhHoSoKyThuatHdr.java
@@ -9,6 +9,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -24,8 +25,14 @@ public class XhHoSoKyThuatHdr extends BaseEntity implements Serializable {
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = XhHoSoKyThuatHdr.TABLE_NAME + "_SEQ")
   @SequenceGenerator(sequenceName = XhHoSoKyThuatHdr.TABLE_NAME + "_SEQ", allocationSize = 1, name = XhHoSoKyThuatHdr.TABLE_NAME + "_SEQ")
   private Long id;
-  private String soHs;
-  private Long idHsktNh;
+  private String soHskt;
+  private Long idHskt;
+  private Long idBbLayMau;
+  private String soBbLayMau;
+  private String soBbLayMauNh;
+  private String soQdGiaoNvNh;
+private LocalDate ngayTaoHskt;
+private LocalDate ngayDuyetHskt;
   private String maDvi;
   private String maDiaDiem;
   private String loaiVthh;
