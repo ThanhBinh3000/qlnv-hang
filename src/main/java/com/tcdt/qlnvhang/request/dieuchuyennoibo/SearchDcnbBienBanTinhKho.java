4b762f1fe70292b956267f36f9ccabf0eed843c6
PM <anhpp@tecapro.com.vn>
2023-07-01 14:22:08 +0700

    fix bug them api danh sach bien ban tinh kho

--- src/main/java/com/tcdt/qlnvhang/request/dieuchuyennoibo/SearchDcnbBienBanTinhKho.java
+++ src/main/java/com/tcdt/qlnvhang/request/dieuchuyennoibo/SearchDcnbBienBanTinhKho.java
@@ -19,6 +19,8 @@ public class SearchDcnbBienBanTinhKho extends BaseRequest {
     private String dViKiemNghiem;
     private LocalDate tuNgay;
     private LocalDate denNgay;
+    private String maLoKho;
+    private String maNganKho;
     private String maDvi;
     private String loaiDc;
     private String loaiQdinh;
