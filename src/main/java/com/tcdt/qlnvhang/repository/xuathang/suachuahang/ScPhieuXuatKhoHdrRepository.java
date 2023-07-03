fe68fa9d721a5a9b2110dad68ea23b3605d0bbe8
NGOLAM\nglam <nlam@hyperion.co.jp>
2023-07-03 14:42:46 +0700

    dev phieu xuat kho + bang ke vt

--- src/main/java/com/tcdt/qlnvhang/repository/xuathang/suachuahang/ScPhieuXuatKhoHdrRepository.java
+++ src/main/java/com/tcdt/qlnvhang/repository/xuathang/suachuahang/ScPhieuXuatKhoHdrRepository.java
@@ -1,6 +1,7 @@
 package com.tcdt.qlnvhang.repository.xuathang.suachuahang;
 
 import com.tcdt.qlnvhang.request.suachua.ScPhieuXuatKhoReq;
+import com.tcdt.qlnvhang.response.suachua.ScPhieuXuatKhoDTO;
 import com.tcdt.qlnvhang.table.xuathang.suachuahang.ScPhieuXuatKhoHdr;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,9 +14,14 @@ import java.util.Optional;
 public interface ScPhieuXuatKhoHdrRepository extends JpaRepository<ScPhieuXuatKhoHdr, Long> {
     Optional<ScPhieuXuatKhoHdr> findBySoPhieuXuatKho(String soPhieuXuatKho);
 
-    @Query(value = "SELECT a FROM ScPhieuXuatKhoHdr a WHERE 1=1 " +
-            "AND (:#{#param.nam} IS NULL OR a.nam = :#{#param.nam})" +
-            "AND (:#{#param.soQdGiaoNv} IS NULL OR a.soQdGiaoNv = :#{#param.soQdGiaoNv})" +
-            "AND (:#{#param.soPhieuXuatKho} IS NULL OR a.soPhieuXuatKho = :#{#param.soPhieuXuatKho})")
-    Page<ScPhieuXuatKhoHdr> searchPage(@Param("param") ScPhieuXuatKhoReq req, Pageable pageable);
+    @Query(value = "SELECT new com.tcdt.qlnvhang.response.suachua.ScPhieuXuatKhoDTO(" +
+            "pxkh.id , qd.id, qd.soQd, qd.nam, qd.thoiHanXuat,pxkh.maDiemKho, pxkh.tenDiemKho, pxkh.maLoKho, pxkh.tenLoKho," +
+            "pxkh.soPhieuXuatKho, pxkh.ngayXuatKho, pxkh.soPhieuKiemDinhChatLuong, pxkh.ngayKyQdGiaoNv, pxkh.trangThai " +
+            ") FROM ScQuyetDinhXuatHang qd " +
+            "LEFT JOIN ScPhieuXuatKhoHdr pxkh on pxkh.soQdGiaoNv = qd.soQd " +
+            "LEFT JOIN ScPhieuXuatKhoDtl pxkd on pxkd.hdrId = pxkh.id WHERE 1=1 " +
+            "AND (:#{#param.nam} IS NULL OR qd.nam = :#{#param.nam})" +
+            "AND (:#{#param.soQdGiaoNv} IS NULL OR qd.soQd = :#{#param.soQdGiaoNv})" +
+            "AND (:#{#param.soPhieuXuatKho} IS NULL OR pxkh.soPhieuXuatKho = :#{#param.soPhieuXuatKho})")
+    Page<ScPhieuXuatKhoDTO> searchPage(@Param("param") ScPhieuXuatKhoReq req, Pageable pageable);
 }
