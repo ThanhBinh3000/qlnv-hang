c75d1fd6baa783c268ebf2706d718e47aa048e36
PM <anhpp@tecapro.com.vn>
2023-07-08 15:15:44 +0700

    biên bản hao dôi bdg

--- src/main/java/com/tcdt/qlnvhang/request/xuathang/daugia/xuatkho/SearchXhDgBbTinhKho.java
+++ src/main/java/com/tcdt/qlnvhang/request/xuathang/daugia/xuatkho/SearchXhDgBbTinhKho.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 public class SearchXhDgBbTinhKho extends BaseRequest {
   private String maDvi;
   private String dvql;
-  private Long nam;
+  private Integer nam;
   private String soQdGiaoNvXh;
   private String soBbTinhKho;
   private LocalDate ngayBatDauXuatTu;
