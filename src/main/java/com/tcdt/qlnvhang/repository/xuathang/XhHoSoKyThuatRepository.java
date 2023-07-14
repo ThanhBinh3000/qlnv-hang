e54abb2749f419a532326b643ddcc20d7a0a6c11
ta quang chien <tqchien1990@gmail.com>
2023-07-14 14:09:40 +0700

    update ho so ky thuat

--- src/main/java/com/tcdt/qlnvhang/repository/xuathang/XhHoSoKyThuatRepository.java
+++ src/main/java/com/tcdt/qlnvhang/repository/xuathang/XhHoSoKyThuatRepository.java
@@ -3,15 +3,12 @@ package com.tcdt.qlnvhang.repository.xuathang;
 import com.tcdt.qlnvhang.request.xuathang.SearchHoSoKyThuatReq;
 import com.tcdt.qlnvhang.response.xuathang.NhHoSoKyThuatDTO;
 import com.tcdt.qlnvhang.table.xuathang.hosokythuat.XhHoSoKyThuatHdr;
-import com.tcdt.qlnvhang.table.xuathang.xuatcuutrovientroxuatcap.xuatcap.XhCtvtQdXuatCapHdr;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface XhHoSoKyThuatRepository extends JpaRepository<XhHoSoKyThuatHdr, Long> {
@@ -20,7 +17,9 @@ public interface XhHoSoKyThuatRepository extends JpaRepository<XhHoSoKyThuatHdr,
       "hs.id, hs.idQdGiaoNvNh , hs.soQdGiaoNvNh , hs.soBbLayMau , hs.soHd , hs.maDvi , hs.soHoSoKyThuat , hs.nam , hs.idBbLayMauXuat , hs.kqKiemTra , 'DT' , bb.maDiemKho , bb.maNhaKho , bb.maNganKho , bb.maLoKho, hs.ngayTao,xh.kqKiemTra) " +
       "FROM NhHoSoKyThuat hs join BienBanLayMau bb on hs.soBbLayMau = bb.soBienBan " +
       "left join XhHoSoKyThuatHdr xh on xh.idHsktNh = hs.id WHERE 1=1 " +
-      "AND hs.soBbLayMau = bb.soBienBan"
+      "AND (hs.soBbLayMau = bb.soBienBan)" +
+      "AND (:#{#param.type} IS NULL OR xh.type = :#{#param.type}) " +
+      "AND (:#{#param.soHoSoKyThuat} IS NULL OR hs.soHoSoKyThuat LIKE CONCAT('%',LOWER(:#{#param.soHoSoKyThuat}),'%')) "
       /* "AND (:#{#param.dvql} IS NULL OR c.maDvi LIKE CONCAT(:#{#param.dvql},'%')) " +
        "AND (:#{#param.type} IS NULL OR c.type = :#{#param.type}) " +
        "AND (:#{#param.loaiVthh} IS NULL OR c.loaiVthh = :#{#param.loaiVthh}) " +
@@ -38,5 +37,5 @@ public interface XhHoSoKyThuatRepository extends JpaRepository<XhHoSoKyThuatHdr,
 
   XhHoSoKyThuatHdr findByMaDiaDiem(String maDiaDiem);
 
-  XhHoSoKyThuatHdr findByIdHsktNhAndType(Long id,String type);
+  XhHoSoKyThuatHdr findByIdHsktNhAndType(Long id, String type);
 }
