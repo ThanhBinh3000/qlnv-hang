b24f783f7458b7ee6b2b11cf0bc872df1f8bcb73
thonq <thongtv@tecapro.com.vn>
2023-07-15 15:58:32 +0700

    xuất khác:Kiểm tra chất lượng VT, TB trong thời gian bảo hành theo HĐ

--- src/main/java/com/tcdt/qlnvhang/request/xuathang/xuatkhac/ktvattubaohanh/XhXkVtBhQdGiaonvXhRequest.java
+++ src/main/java/com/tcdt/qlnvhang/request/xuathang/xuatkhac/ktvattubaohanh/XhXkVtBhQdGiaonvXhRequest.java
@@ -29,6 +29,7 @@ public class XhXkVtBhQdGiaonvXhRequest extends BaseRequest {
   LocalDate ngayPduyet;
   Long nguoiPduyetId;
   String lyDoTuChoi;
+  private String loaCanCu;
   List<String> listTrangThaiXh = new ArrayList<>();
   Integer capDvi;
   List<XhXkVtBhQdGiaonvXhDtl> qdGiaonvXhDtl = new ArrayList<>();
