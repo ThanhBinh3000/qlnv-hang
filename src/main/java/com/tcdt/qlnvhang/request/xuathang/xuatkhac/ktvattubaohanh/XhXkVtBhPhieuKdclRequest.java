9c103d8ed2e948ac5025c452112ea6bcd69f7f0f
thonq <thongtv@tecapro.com.vn>
2023-07-21 15:17:05 +0700

    xuất khác:KTCL VT, TB trong thời gian bảo hành theo HĐ> KTCL > Phiếu kiểm định chất lượng

---
+++ src/main/java/com/tcdt/qlnvhang/request/xuathang/xuatkhac/ktvattubaohanh/XhXkVtBhPhieuKdclRequest.java
@@ -0,0 +1,50 @@
+package com.tcdt.qlnvhang.request.xuathang.xuatkhac.ktvattubaohanh;
+
+import com.tcdt.qlnvhang.request.BaseRequest;
+import com.tcdt.qlnvhang.request.object.FileDinhKemReq;
+import com.tcdt.qlnvhang.table.xuathang.xuatkhac.ktvattu.XhXkVtPhieuKdclDtl;
+import com.tcdt.qlnvhang.table.xuathang.xuatkhac.ktvattubaohanh.XhXkVtBhPhieuKdclDtl;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class XhXkVtBhPhieuKdclRequest extends BaseRequest {
+    private Long id;
+    private Integer nam;
+    private String maDvi;
+    private String maQhNs;
+    private Long idQdGiaoNvXh;
+    private String soQdGiaoNvXh;
+    private Long idBbLayMau;
+    private Integer soLanLm;
+    private String soBbLayMau;
+    private LocalDate ngayLauMau;
+    private String soPhieu;
+    private LocalDate ngayLapPhieu;
+    private LocalDate ngayKiemDinh;
+    private String dviKiemNghiem;
+    private String loaiVthh;
+    private String cloaiVthh;
+    private String maDiaDiem;
+    private String ppLayMau;
+    private String nhanXetKetLuan;
+    private Boolean isDat;
+    private Boolean mauBiHuy;
+    private String trangThai;
+    private LocalDate ngayGduyet;
+    private Long nguoiGduyetId;
+    private LocalDate ngayPduyet;
+    private Long nguoiPduyetId;
+    private String lyDoTuChoi;
+    private String type;
+    private List<FileDinhKemReq> fileDinhKems = new ArrayList<>();
+    private List<XhXkVtBhPhieuKdclDtl> phieuKdclDtl = new ArrayList<>();
+
+    //search params
+    private LocalDate ngayKiemDinhTu;
+    private LocalDate ngayKiemDinhDen;
+    private String dvql;
+}
---
+++ src/main/java/com/tcdt/qlnvhang/table/xuathang/xuatkhac/ktvattubaohanh/XhXkVtBhPhieuKdclDtl.java
@@ -0,0 +1,33 @@
+package com.tcdt.qlnvhang.table.xuathang.xuatkhac.ktvattubaohanh;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = XhXkVtBhPhieuKdclDtl.TABLE_NAME)
+@Data
+public class XhXkVtBhPhieuKdclDtl implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    public static final String TABLE_NAME = "XH_XK_VT_BH_PHIEU_KDCL_DTL";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = XhXkVtBhPhieuKdclDtl.TABLE_NAME + "_SEQ")
+    @SequenceGenerator(sequenceName = XhXkVtBhPhieuKdclDtl.TABLE_NAME
+        + "_SEQ", allocationSize = 1, name = XhXkVtBhPhieuKdclDtl.TABLE_NAME + "_SEQ")
+    private Long id;
+    private String tenChiTieu;
+    private String maChiTieu;
+    private String chiSo;
+    private String ketQua;
+    private String ppKiemTra;
+    private String danhGia; // Đạt/ Không đạt
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idHdr")
+    @JsonIgnore
+    private XhXkVtBhPhieuKdclHdr phieuKdclHdr;
+}
