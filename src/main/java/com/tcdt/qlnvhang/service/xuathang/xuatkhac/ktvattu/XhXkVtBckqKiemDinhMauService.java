fa8008ee20a87f12ed0818ae61baf6afa814e0a6
OTHoa <anhpp@tecapro.com.vn>
2023-07-21 17:50:32 +0700

    update xuat khac - phieu kiem dinh chat luong - kiem tra vt trc khi het han luu kho

--- src/main/java/com/tcdt/qlnvhang/service/xuathang/xuatkhac/ktvattu/XhXkVtBckqKiemDinhMauService.java
+++ src/main/java/com/tcdt/qlnvhang/service/xuathang/xuatkhac/ktvattu/XhXkVtBckqKiemDinhMauService.java
@@ -149,31 +149,32 @@ public class XhXkVtBckqKiemDinhMauService extends BaseServiceImpl {
         if (!optional.isPresent()) {
             throw new Exception("Bản ghi không tồn tại");
         }
-        if (!optional.get().getTrangThai().equals(TrangThaiAllEnum.DU_THAO.getId())) {
-            throw new Exception("Bản ghi có trạng thái khác dự thảo, không thể xóa.");
-        }
-        XhXkVtBckqKiemDinhMau data = optional.get();
-        fileDinhKemService.deleteMultiple(Collections.singleton(data.getId()), Collections.singleton(XhXkVtBckqKiemDinhMau.TABLE_NAME));
-        //update mẫu bị hủy cho phiếu xuất kho
-        Long[] idsQdGiaoNvXh = Arrays.stream(data.getIdQdGiaoNvXh().split(","))
-                .map(String::trim)
-                .map(Long::valueOf)
-                .toArray(Long[]::new);
+        if (optional.get().getTrangThai().equals(TrangThaiAllEnum.DU_THAO.getId()) || optional.get().getTrangThai().equals(TrangThaiAllEnum.TU_CHOI_LDC.getId())) {
+            XhXkVtBckqKiemDinhMau data = optional.get();
+            fileDinhKemService.deleteMultiple(Collections.singleton(data.getId()), Collections.singleton(XhXkVtBckqKiemDinhMau.TABLE_NAME));
+            //update mẫu bị hủy cho phiếu xuất kho
+            Long[] idsQdGiaoNvXh = Arrays.stream(data.getIdQdGiaoNvXh().split(","))
+                    .map(String::trim)
+                    .map(Long::valueOf)
+                    .toArray(Long[]::new);
 
-        List<XhXkVtPhieuXuatKho> allByIdCanCuIn = xhXkVtPhieuXuatKhoRepository.findAllByIdCanCuIn(Arrays.asList(idsQdGiaoNvXh));
-        if (!allByIdCanCuIn.isEmpty()) {
-            allByIdCanCuIn.forEach(item -> {
-                item.setMauBiHuy(Boolean.FALSE);
-            });
+            List<XhXkVtPhieuXuatKho> allByIdCanCuIn = xhXkVtPhieuXuatKhoRepository.findAllByIdCanCuIn(Arrays.asList(idsQdGiaoNvXh));
+            if (!allByIdCanCuIn.isEmpty()) {
+                allByIdCanCuIn.forEach(item -> {
+                    item.setMauBiHuy(Boolean.FALSE);
+                });
+            }
+            List<XhXkVtQdGiaonvXhHdr> listQdGiaoNvXh = xhXkVtQdGiaonvXhRepository.findByIdIn(Arrays.asList(idsQdGiaoNvXh));
+            if (!listQdGiaoNvXh.isEmpty()) {
+                listQdGiaoNvXh.forEach(item -> {
+                    item.setSoBaoCaoKdm(null);
+                    item.setIdBaoCaoKdm(null);
+                });
+            }
+            xhXkVtBckqKiemDinhMauRepository.delete(data);
+        } else {
+            throw new Exception("Bản ghi đang ở trạng thái " + TrangThaiAllEnum.getLabelById(optional.get().getTrangThai()) + " không thể xóa.");
         }
-        List<XhXkVtQdGiaonvXhHdr> listQdGiaoNvXh = xhXkVtQdGiaonvXhRepository.findByIdIn(Arrays.asList(idsQdGiaoNvXh));
-        if (!listQdGiaoNvXh.isEmpty()) {
-            listQdGiaoNvXh.forEach(item -> {
-                item.setSoBaoCaoKdm(null);
-                item.setIdBaoCaoKdm(null);
-            });
-        }
-        xhXkVtBckqKiemDinhMauRepository.delete(data);
     }
 
 //    @Transient
