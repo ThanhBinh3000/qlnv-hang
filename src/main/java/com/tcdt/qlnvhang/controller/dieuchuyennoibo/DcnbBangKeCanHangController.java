7f7c15ba0f0cc8814fb98eb876177224bfc3fbeb
PM <anhpp@tecapro.com.vn>
2023-07-31 18:21:28 +0700

    fix bug

--- src/main/java/com/tcdt/qlnvhang/controller/dieuchuyennoibo/DcnbBangKeCanHangController.java
+++ src/main/java/com/tcdt/qlnvhang/controller/dieuchuyennoibo/DcnbBangKeCanHangController.java
@@ -55,6 +55,26 @@ public class DcnbBangKeCanHangController {
         return ResponseEntity.ok(resp);
     }
 
+    @ApiOperation(value = "Tra cứu thông tin ", response = List.class)
+    @PostMapping(value = PathContains.DANH_SACH, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<BaseResponse> list(@CurrentUser CustomUserDetails currentUser,
+                                                  @RequestBody SearchBangKeCanHang objReq) {
+        BaseResponse resp = new BaseResponse();
+        try {
+            resp.setData(dcnbBangKeCanHangServiceImpl.list(currentUser,objReq));
+            resp.setStatusCode(EnumResponse.RESP_SUCC.getValue());
+            resp.setMsg(EnumResponse.RESP_SUCC.getDescription());
+        } catch ( Exception e) {
+            e.printStackTrace();
+            resp.setStatusCode(EnumResponse.RESP_FAIL.getValue());
+            resp.setMsg(e.getMessage());
+            log.error("Tra cứu thông tin : {}", e);
+        }
+
+        return ResponseEntity.ok(resp);
+    }
+
     @ApiOperation(value = "Tạo mới thông tin  ", response = List.class)
     @PostMapping(value =  PathContains.URL_TAO_MOI, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
