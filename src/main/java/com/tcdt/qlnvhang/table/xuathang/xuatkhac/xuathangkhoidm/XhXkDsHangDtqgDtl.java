405fafad0281b919257fac9161165e18181374b4
OTHoa <anhpp@tecapro.com.vn>
2023-08-07 17:14:39 +0700

    update xuat khac -Xuat hang khoi danh muc

---
+++ src/main/java/com/tcdt/qlnvhang/table/xuathang/xuatkhac/xuathangkhoidm/XhXkDsHangDtqgDtl.java
@@ -0,0 +1,41 @@
+package com.tcdt.qlnvhang.table.xuathang.xuatkhac.xuathangkhoidm;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.tcdt.qlnvhang.table.FileDinhKem;
+import com.tcdt.qlnvhang.util.DataUtils;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@Entity
+@Table(name = XhXkDsHangDtqgDtl.TABLE_NAME)
+public class XhXkDsHangDtqgDtl {
+
+    public static final String TABLE_NAME = "XH_XK_DS_HANG_DTQG_DTL";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "XH_XK_DS_HANG_DTQG_DTL_SEQ")
+    @SequenceGenerator(sequenceName = "XH_XK_DS_HANG_DTQG_DTL_SEQ", allocationSize = 1, name = "XH_XK_DS_HANG_DTQG_DTL_SEQ")
+    private Long id;
+    private String ma;
+    private String ten;
+    private String maCha;
+    private String maDviTinh;
+    private String cap;
+    private String loaiHang;
+    private String dviQly;
+    private Boolean isNgoaiDanhMuc;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idHdr")
+    @JsonIgnore
+    private XhXkDsHangDtqgHdr xhXkDsHangDtqgHdr;
+
+}
