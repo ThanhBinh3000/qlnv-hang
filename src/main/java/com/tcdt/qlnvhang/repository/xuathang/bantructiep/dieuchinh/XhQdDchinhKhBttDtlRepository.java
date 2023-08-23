a93d95ad091bd115489072f9d0b5f6afb296e209
TTBinh <anhpp@tecapro.com.vn>
2023-08-23 19:10:31 +0700

    điều chỉnh Kh bán trực tiếp

--- src/main/java/com/tcdt/qlnvhang/repository/xuathang/bantructiep/dieuchinh/XhQdDchinhKhBttDtlRepository.java
+++ src/main/java/com/tcdt/qlnvhang/repository/xuathang/bantructiep/dieuchinh/XhQdDchinhKhBttDtlRepository.java
@@ -1,7 +1,6 @@
 package com.tcdt.qlnvhang.repository.xuathang.bantructiep.dieuchinh;
 
 import com.tcdt.qlnvhang.entities.xuathang.bantructiep.dieuchinh.XhQdDchinhKhBttDtl;
-import com.tcdt.qlnvhang.entities.xuathang.daugia.quyetdinhdieuchinhbdg.XhQdDchinhKhBdgDtl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +9,9 @@ import java.util.List;
 @Repository
 public interface XhQdDchinhKhBttDtlRepository extends JpaRepository<XhQdDchinhKhBttDtl, Long> {
 
-    void deleteAllByIdDcHdr(Long idDcHdr);
+    void deleteAllByIdHdr(Long idHdr);
 
-    List<XhQdDchinhKhBttDtl> findAllByIdDcHdr(Long idDcHdr);
+    List<XhQdDchinhKhBttDtl> findAllByIdHdr(Long idHdr);
 
+    List<XhQdDchinhKhBttDtl> findByIdHdrIn(List<Long> listId);
 }
