24542969d25eb23e74f05c5d0662adf683c25a21
ta quang chien <tqchien1990@gmail.com>
2023-08-25 11:59:39 +0700

    update ctvt qdgnv

--- src/main/java/com/tcdt/qlnvhang/request/xuathang/xuatcuutrovientroxuatcap/xuatcuutrovientro/XhCtvtQuyetDinhGnvHdrReq.java
+++ src/main/java/com/tcdt/qlnvhang/request/xuathang/xuatcuutrovientroxuatcap/xuatcuutrovientro/XhCtvtQuyetDinhGnvHdrReq.java
@@ -3,7 +3,7 @@ package com.tcdt.qlnvhang.request.xuathang.xuatcuutrovientroxuatcap.xuatcuutrovi
 import com.tcdt.qlnvhang.table.xuathang.xuatcuutrovientroxuatcap.xuatcuutrovientro.XhCtvtQuyetDinhGnvHdr;
 import lombok.Data;
 
-@Data
+
 public class XhCtvtQuyetDinhGnvHdrReq extends XhCtvtQuyetDinhGnvHdr {
 
 }
