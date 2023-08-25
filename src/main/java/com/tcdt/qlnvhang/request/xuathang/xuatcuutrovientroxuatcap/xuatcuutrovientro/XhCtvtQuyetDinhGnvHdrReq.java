e383e8ccb812e769cda3a6d2e94b82d647ff0258
ta quang chien <tqchien1990@gmail.com>
2023-08-25 14:36:11 +0700

    update ctvt qdgnv

--- src/main/java/com/tcdt/qlnvhang/request/xuathang/xuatcuutrovientroxuatcap/xuatcuutrovientro/XhCtvtQuyetDinhGnvHdrReq.java
+++ src/main/java/com/tcdt/qlnvhang/request/xuathang/xuatcuutrovientroxuatcap/xuatcuutrovientro/XhCtvtQuyetDinhGnvHdrReq.java
@@ -1,9 +1,56 @@
 package com.tcdt.qlnvhang.request.xuathang.xuatcuutrovientroxuatcap.xuatcuutrovientro;
 
-import com.tcdt.qlnvhang.table.xuathang.xuatcuutrovientroxuatcap.xuatcuutrovientro.XhCtvtQuyetDinhGnvHdr;
+import com.tcdt.qlnvhang.entities.FileDinhKemJoinTable;
+import com.tcdt.qlnvhang.table.xuathang.xuatcuutrovientroxuatcap.xuatcuutrovientro.XhCtvtQdGiaoNvXhDtl;
+import com.tcdt.qlnvhang.table.xuathang.xuatcuutrovientroxuatcap.xuatcuutrovientro.XhCtvtQuyetDinhGnvDtl;
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
-public class XhCtvtQuyetDinhGnvHdrReq extends XhCtvtQuyetDinhGnvHdr {
-
+@Data
+public class XhCtvtQuyetDinhGnvHdrReq {
+  LocalDateTime ngayTao;
+  Long nguoiTaoId;
+  LocalDateTime ngaySua;
+  Long nguoiSuaId;
+  Long id;
+  String maDvi;
+  Integer nam;
+  String soBbQd;
+  LocalDate ngayKy;
+  Long idQdPd;
+  String soQdPd;
+  String loaiVthh;
+  String cloaiVthh;
+  String tenVthh;
+  BigDecimal soLuong;
+  LocalDate thoiGianGiaoNhan;
+  String trichYeu;
+  String trangThai;
+  String lyDoTuChoi;
+  String trangThaiXh;
+  String soBbHaoDoi;
+  String soBbTinhKho;
+  BigDecimal tongSoLuong;
+  BigDecimal thanhTien;
+  String type;
+  LocalDate ngayGduyet;
+  Long nguoiGduyetId;
+  LocalDate ngayPduyet;
+  Long nguoiPduyetId;
+  String loaiNhapXuat;
+  String kieuNhapXuat;
+  String mucDichXuat;
+  String tenDvi;
+  String tenLoaiVthh;
+  String tenCloaiVthh;
+  String tenTrangThai;
+  String tenTrangThaiXh;
+  List<XhCtvtQdGiaoNvXhDtl> noiDungCuuTro;
+  List<XhCtvtQuyetDinhGnvDtl> children;
+  List<FileDinhKemJoinTable> fileDinhKem;
+  List<FileDinhKemJoinTable> canCu;
 }
