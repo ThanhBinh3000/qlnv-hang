c92c25943a03eb13c33804fe84d97b0550084cef
VuxNT <vunt1809@gmail.com>
2023-09-13 14:28:50 +0700

    Print preview nhap khac

--- src/main/java/com/tcdt/qlnvhang/controller/nhaphang/nhapkhac/HhQdGiaoNvNhapKhacHdrController.java
+++ src/main/java/com/tcdt/qlnvhang/controller/nhaphang/nhapkhac/HhQdGiaoNvNhapKhacHdrController.java
@@ -230,4 +230,20 @@ public class HhQdGiaoNvNhapKhacHdrController {
             mapper.writeValue(response.getOutputStream(), body);
         }
     }
+
+    @ApiOperation(value = "Xem truoc", response = List.class)
+    @PostMapping(value = PathContains.URL_XEM_TRUOC, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<BaseResponse> preview(@RequestBody HhQdGiaoNvuNhapKhacSearch req) {
+        BaseResponse resp = new BaseResponse();
+        try {
+            resp.setData(service.preview(req));
+            resp.setStatusCode(EnumResponse.RESP_SUCC.getValue());
+            resp.setMsg(EnumResponse.RESP_SUCC.getDescription());
+        } catch (Exception e) {
+            resp.setStatusCode(EnumResponse.RESP_FAIL.getValue());
+            resp.setMsg(e.getMessage());
+        }
+        return ResponseEntity.ok(resp);
+    }
 }
