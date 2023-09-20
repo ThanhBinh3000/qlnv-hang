0d4f4484db1013fcc4b34c1eb557683455b65d1f
VuxNT <vunt1809@gmail.com>
2023-09-20 22:19:55 +0700

    fix xuat hang - thanh ly

--- src/main/java/com/tcdt/qlnvhang/table/xuathang/thanhlytieuhuy/thanhly/XhTlTongHopDtl.java
+++ src/main/java/com/tcdt/qlnvhang/table/xuathang/thanhlytieuhuy/thanhly/XhTlTongHopDtl.java
@@ -2,6 +2,7 @@ package com.tcdt.qlnvhang.table.xuathang.thanhlytieuhuy.thanhly;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.tcdt.qlnvhang.entities.BaseEntity;
+import com.tcdt.qlnvhang.table.xuathang.suachuahang.ScDanhSachHdr;
 import com.tcdt.qlnvhang.util.DataUtils;
 import lombok.Data;
 
@@ -24,81 +25,7 @@ public class XhTlTongHopDtl extends BaseEntity implements Serializable {
   private Long id;
   private Long idTongHop;
   private Long idDsHdr;
-  private String maTongHop;
-  private String maDiaDiem;
-  private String loaiVthh;
-  private String cloaiVthh;
-  private String donViTinh;
-  private BigDecimal slHienTai;
-  private BigDecimal slDeXuat;
-  private BigDecimal slDaDuyet;
-  private BigDecimal thanhTien;
-  private LocalDate ngayNhapKho;
-  private LocalDate ngayDeXuat;
-  private LocalDate ngayTongHop;
-  private String lyDo;
-  private String type;
-
-  @JsonIgnore
   @Transient
-  private Map<String, String> mapVthh;
-  @Transient
-  private String tenLoaiVthh;
-  @Transient
-  private String tenCloaiVthh;
-  @JsonIgnore
-  @Transient
-  private Map<String, String> mapDmucDvi;
-  @Transient
-  private String tenCuc;
-  @Transient
-  private String tenChiCuc;
-  @Transient
-  private String tenDiemKho;
-  @Transient
-  private String tenNhaKho;
-  @Transient
-  private String tenNganKho;
-  @Transient
-  private String tenLoKho;
-
-  public void setMapDmucDvi(Map<String, String> mapDmucDvi) {
-    this.mapDmucDvi = mapDmucDvi;
-    if (!DataUtils.isNullObject(getMaDiaDiem())) {
-      String maCuc = getMaDiaDiem().length() >= 6 ? getMaDiaDiem().substring(0, 6) : "";
-      String maChiCuc = getMaDiaDiem().length() >= 8 ? getMaDiaDiem().substring(0, 8) : "";
-      String maDiemKho = getMaDiaDiem().length() >= 10 ? getMaDiaDiem().substring(0, 10) : "";
-      String maNhaKho = getMaDiaDiem().length() >= 12 ? getMaDiaDiem().substring(0, 12) : "";
-      String maNganKho = getMaDiaDiem().length() >= 14 ? getMaDiaDiem().substring(0, 14) : "";
-      String maLoKho = getMaDiaDiem().length() >= 16 ? getMaDiaDiem().substring(0, 16) : "";
-      String tenCuc = mapDmucDvi.containsKey(maCuc) ? mapDmucDvi.get(maCuc) : null;
-      String tenChiCuc = mapDmucDvi.containsKey(maChiCuc) ? mapDmucDvi.get(maChiCuc) : null;
-      String tenDiemKho = mapDmucDvi.containsKey(maDiemKho) ? mapDmucDvi.get(maDiemKho) : null;
-      String tenNhaKho = mapDmucDvi.containsKey(maNhaKho) ? mapDmucDvi.get(maNhaKho) : null;
-      String tenNganKho = mapDmucDvi.containsKey(maNganKho) ? mapDmucDvi.get(maNganKho) : null;
-      String tenLoKho = mapDmucDvi.containsKey(maLoKho) ? mapDmucDvi.get(maLoKho) : null;
-      setTenCuc(tenCuc);
-      setTenChiCuc(tenChiCuc);
-      setTenDiemKho(tenDiemKho);
-      setTenNhaKho(tenNhaKho);
-      setTenNganKho(tenNganKho);
-      setTenLoKho(tenLoKho);
-    }
-  }
-
-  public void setMapVthh(Map<String, String> mapVthh) {
-    this.mapVthh = mapVthh;
-    if (!DataUtils.isNullObject(getLoaiVthh())) {
-      setTenLoaiVthh(mapVthh.containsKey(getLoaiVthh()) ? mapVthh.get(getLoaiVthh()) : null);
-    }
-    if (!DataUtils.isNullObject(getCloaiVthh())) {
-      setTenCloaiVthh(mapVthh.containsKey(getCloaiVthh()) ? mapVthh.get(getCloaiVthh()) : null);
-    }
-  }
-
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "idHdr")
-  @JsonIgnore
-  private XhTlTongHopHdr tongHopHdr;
+  private XhTlDanhSachHdr xhTlDanhSachHdr;
 
 }
--- src/main/java/com/tcdt/qlnvhang/table/xuathang/thanhlytieuhuy/thanhly/XhTlTongHopHdr.java
+++ src/main/java/com/tcdt/qlnvhang/table/xuathang/thanhlytieuhuy/thanhly/XhTlTongHopHdr.java
@@ -1,6 +1,7 @@
 package com.tcdt.qlnvhang.table.xuathang.thanhlytieuhuy.thanhly;
 
 import com.tcdt.qlnvhang.entities.BaseEntity;
+import com.tcdt.qlnvhang.enums.TrangThaiAllEnum;
 import com.tcdt.qlnvhang.table.FileDinhKem;
 import com.tcdt.qlnvhang.util.DataUtils;
 import lombok.Data;
@@ -12,6 +13,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -24,8 +26,8 @@ public class XhTlTongHopHdr extends BaseEntity implements Serializable {
   public static final String TABLE_NAME = "XH_TL_TONG_HOP_HDR";
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = XhTlTongHopHdr.TABLE_NAME + "_SEQ")
-  @SequenceGenerator(sequenceName = XhTlTongHopHdr.TABLE_NAME + "_SEQ", allocationSize = 1, name = XhTlTongHopHdr.TABLE_NAME + "_SEQ")
+//  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = XhTlTongHopHdr.TABLE_NAME + "_SEQ")
+//  @SequenceGenerator(sequenceName = XhTlTongHopHdr.TABLE_NAME + "_SEQ", allocationSize = 1, name = XhTlTongHopHdr.TABLE_NAME + "_SEQ")
   private Long id;
   private Integer nam;
   private String maDvi;
@@ -34,37 +36,20 @@ public class XhTlTongHopHdr extends BaseEntity implements Serializable {
   private LocalDate thoiGianTlTu;
   private LocalDate thoiGianTlDen;
   private String trangThai;
-  private String trangThaiTl;
-  private Long idHoSo;
-  private String soHoSo;
-  private Long idQdPd;
-  private String soQdPd;
-  private LocalDate ngayKyQd;
   private LocalDate ngayGduyet;
   private Long nguoiGduyetId;
-  private LocalDate ngayPduyet;
-  private Long nguoiPduyetId;
-  private String lyDoTuChoi;
-  private BigDecimal tongSlHienTai;
-  private BigDecimal tongSlDeXuat;
-  private BigDecimal tongSlDaDuyet;
-
   @Transient
   private String tenTrangThai;
-  @Transient
-  private String tenTrangThaiTl;
-
   @Transient
   private String tenDvi;
 
   @Transient
-  private String maDvql;
-  @Transient
-  private String tenDvql;
-
-  @OneToMany(mappedBy = "tongHopHdr", cascade = CascadeType.ALL)
-  private List<XhTlTongHopDtl> tongHopDtl = new ArrayList<>();
+  private List<XhTlTongHopDtl> children = new ArrayList<>();
 
   @Transient
   private List<FileDinhKem> fileDinhKem = new ArrayList<>();
+
+  public String getTenTrangThai(){
+    return TrangThaiAllEnum.getLabelById(getTrangThai());
+  }
 }
