3c4624b69b039e5cc5ad2cccdde46506a0a3b442
OTHoa <anhpp@tecapro.com.vn>
2023-10-25 16:24:08 +0700

    update xuat khác - preview

--- src/main/java/com/tcdt/qlnvhang/controller/khoahoccongnghebaoquan/KhCnCongTrinhNghienCuuController.java
+++ src/main/java/com/tcdt/qlnvhang/controller/khoahoccongnghebaoquan/KhCnCongTrinhNghienCuuController.java
@@ -63,6 +63,7 @@ public class KhCnCongTrinhNghienCuuController extends BaseController {
             resp.setStatusCode(EnumResponse.RESP_SUCC.getValue());
             resp.setMsg(EnumResponse.RESP_SUCC.getDescription());
         } catch (Exception e) {
+            e.printStackTrace();
             resp.setStatusCode(EnumResponse.RESP_FAIL.getValue());
             resp.setMsg(e.getMessage());
             log.error("Tạo mới: {}", e);
