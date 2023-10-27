6d24bfa4b0e3568b5b6a04338898f078ca95d77b
PM <anhpp@tecapro.com.vn>
2023-10-27 11:00:25 +0700

    fix bug

---
+++ src/main/java/com/tcdt/qlnvhang/dto/XhCtvtQuyetDinhPdHdrPreviewDTO.java
@@ -0,0 +1,14 @@
+package com.tcdt.qlnvhang.dto;
+
+import lombok.Data;
+
+@Data
+public class XhCtvtQuyetDinhPdHdrPreviewDTO {
+    private String stt;
+    private String tenCuc;
+    private String tenNoiNhan;
+    private String tenChungLoaiHang;
+    private Long soLuongCucThucHien;
+    private String mucDichXuatCucTro;
+    private String thoiHanXuatCuuTro;
+}
