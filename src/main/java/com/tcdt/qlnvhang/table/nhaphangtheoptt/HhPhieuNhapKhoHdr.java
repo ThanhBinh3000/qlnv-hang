8ce02150a07ff46f6bc951a17cf359806736e8e3
PM <anhpp@tecapro.com.vn>
2023-11-23 16:52:02 +0700

    fix bug

--- src/main/java/com/tcdt/qlnvhang/table/nhaphangtheoptt/HhPhieuNhapKhoHdr.java
+++ src/main/java/com/tcdt/qlnvhang/table/nhaphangtheoptt/HhPhieuNhapKhoHdr.java
@@ -148,10 +148,12 @@ public class HhPhieuNhapKhoHdr implements Serializable {
     private HhBcanKeHangHdr hhBcanKeHangHdr;
 
 
-
-
-
-
-
-
+    @Transient
+    private BigDecimal tongSl;
+    @Transient
+    private String tongSlBc;
+    @Transient
+    private BigDecimal tongSt;
+    @Transient
+    private String tongStBc;
 }
