f868be6dcb1073f501e1ebcce270299bf7412f5e
DuongVD <duongvd@ttkd6.com>
2023-12-05 11:47:27 +0700

    *fix nhap dt

--- src/main/java/com/tcdt/qlnvhang/service/nhaphang/bbanlaymau/BienBanLayMauService.java
+++ src/main/java/com/tcdt/qlnvhang/service/nhaphang/bbanlaymau/BienBanLayMauService.java
@@ -5,7 +5,12 @@ import com.tcdt.qlnvhang.request.object.bbanlaymau.BienBanLayMauReq;
 import com.tcdt.qlnvhang.request.object.vattu.bienbanguihang.NhBienBanGuiHangReq;
 import com.tcdt.qlnvhang.service.BaseService;
 import com.tcdt.qlnvhang.table.ReportTemplateResponse;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface BienBanLayMauService extends BaseService<BienBanLayMau,BienBanLayMauReq,Long> {
     ReportTemplateResponse preview(BienBanLayMauReq req) throws Exception;
+
+    List<BienBanLayMau> searchList(BienBanLayMauReq objReq);
 }
